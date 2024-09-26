b90eafd31d4eeb67ca1e442cdfaa9fc72c469a30
baconpaul <paul@baconpaul.org>
2024-09-26 21:59:26 +0000

    Update doxygen for shortcircuit-xt from github action

--- docs/shortcircuit-xt/search/defines_1.js
+++ docs/shortcircuit-xt/search/defines_1.js
@@ -1,10 +1,10 @@
 var searchData=
 [
-  ['c_7567',['C',['../ColorMap_8cpp.html#ac54ae397901fe700628cafadea3c5208',1,'ColorMap.cpp']]],
-  ['call_5froute_7568',['CALL_ROUTE',['../group_8cpp.html#a9bc8ccb442e2bdec2f1c9d197bf0020b',1,'CALL_ROUTE():&#160;group.cpp'],['../voice_8cpp.html#a9bc8ccb442e2bdec2f1c9d197bf0020b',1,'CALL_ROUTE():&#160;voice.cpp']]],
-  ['check_7569',['CHECK',['../exs__import_8cpp.html#a836cca6b6ed3a1706353f6d1bca0935f',1,'exs_import.cpp']]],
-  ['client_5fserial_5frequest_5fresponse_7570',['CLIENT_SERIAL_REQUEST_RESPONSE',['../client__macros_8h.html#a82e0a43b87b43c4defe2d1021c1151e7',1,'client_macros.h']]],
-  ['client_5fto_5fserial_7571',['CLIENT_TO_SERIAL',['../client__macros_8h.html#a52585256172848e461cc1add3b648533',1,'client_macros.h']]],
-  ['client_5fto_5fserial_5fconstrained_7572',['CLIENT_TO_SERIAL_CONSTRAINED',['../client__macros_8h.html#ac4ab44a77496575b19fa7e8d697d247f',1,'client_macros.h']]],
-  ['cs_7573',['CS',['../PartEffectsPane_8cpp.html#a1178db9855c046720f7b764ce8caafad',1,'PartEffectsPane.cpp']]]
+  ['c_7570',['C',['../ColorMap_8cpp.html#ac54ae397901fe700628cafadea3c5208',1,'ColorMap.cpp']]],
+  ['call_5froute_7571',['CALL_ROUTE',['../group_8cpp.html#a9bc8ccb442e2bdec2f1c9d197bf0020b',1,'CALL_ROUTE():&#160;group.cpp'],['../voice_8cpp.html#a9bc8ccb442e2bdec2f1c9d197bf0020b',1,'CALL_ROUTE():&#160;voice.cpp']]],
+  ['check_7572',['CHECK',['../exs__import_8cpp.html#a836cca6b6ed3a1706353f6d1bca0935f',1,'exs_import.cpp']]],
+  ['client_5fserial_5frequest_5fresponse_7573',['CLIENT_SERIAL_REQUEST_RESPONSE',['../client__macros_8h.html#a82e0a43b87b43c4defe2d1021c1151e7',1,'client_macros.h']]],
+  ['client_5fto_5fserial_7574',['CLIENT_TO_SERIAL',['../client__macros_8h.html#a52585256172848e461cc1add3b648533',1,'client_macros.h']]],
+  ['client_5fto_5fserial_5fconstrained_7575',['CLIENT_TO_SERIAL_CONSTRAINED',['../client__macros_8h.html#ac4ab44a77496575b19fa7e8d697d247f',1,'client_macros.h']]],
+  ['cs_7576',['CS',['../PartEffectsPane_8cpp.html#a1178db9855c046720f7b764ce8caafad',1,'PartEffectsPane.cpp']]]
 ];
--- docs/shortcircuit-xt/search/enumvalues_4.js
+++ docs/shortcircuit-xt/search/enumvalues_4.js
@@ -1,13 +1,13 @@
 var searchData=
 [
-  ['effect_7282',['effect',['../structscxt_1_1sfz__support_1_1SFZParser_1_1Header.html#a1d1cfd8ffb84e947f82999c682b666a7a501f9ea46247fec3e513f1a49f2d548a',1,'scxt::sfz_support::SFZParser::Header']]],
-  ['endl_7283',['endL',['../structscxt_1_1ui_1_1app_1_1edit__screen_1_1VariantDisplay.html#a8c72c82f024ad6af2e49918de816504fabf9e9fb3920d2d8a5a87983d9dfa3f94',1,'scxt::ui::app::edit_screen::VariantDisplay']]],
-  ['endp_7284',['endP',['../structscxt_1_1ui_1_1app_1_1edit__screen_1_1VariantDisplay.html#a8c72c82f024ad6af2e49918de816504fadb49e7f174779d056a7d0c4a0ddb40bf',1,'scxt::ui::app::edit_screen::VariantDisplay']]],
-  ['endpoints_7285',['ENDPOINTS',['../structscxt_1_1engine_1_1Zone.html#a102a252ae2c383d786df8136d4af2a48af177f01b411631e4ec5f79071dbfb7e7',1,'scxt::engine::Zone']]],
-  ['engine_5fgroup_7286',['engine_Group',['../structscxt_1_1messaging_1_1audio_1_1AudioToSerialization_1_1ToBeDeleted.html#a5a8b2dc9e45a9ee81a94ef304fb62505ac77866631b1fcd2d68f01b7654d10dd1',1,'scxt::messaging::audio::AudioToSerialization::ToBeDeleted']]],
-  ['engine_5fzone_7287',['engine_Zone',['../structscxt_1_1messaging_1_1audio_1_1AudioToSerialization_1_1ToBeDeleted.html#a5a8b2dc9e45a9ee81a94ef304fb62505abb2e7082ad16486360031ff336659e74',1,'scxt::messaging::audio::AudioToSerialization::ToBeDeleted']]],
-  ['env_7288',['ENV',['../structscxt_1_1modulation_1_1shared_1_1HasModulators.html#a9b03541840ed282f2687903729ec6c08a90a589408899045d76b41b0b3520afaf',1,'scxt::modulation::shared::HasModulators']]],
-  ['error_5fbus_7289',['ERROR_BUS',['../namespacescxt_1_1engine.html#abda9c7d4255cb9369a2cc3208fb3dec8abc5f80341be466fd1044ceb745d8d9f8',1,'scxt::engine']]],
-  ['expecting_5findex_7290',['EXPECTING_INDEX',['../structscxt_1_1ui_1_1connectors_1_1JSONLayoutConsumer.html#a5d74787dedbc4e11c1ab15bf487e61f8a1ccac9958f721a71a57e82cce3f39ec8',1,'scxt::ui::connectors::JSONLayoutConsumer']]],
-  ['expression_7291',['EXPRESSION',['../structscxt_1_1voice_1_1Voice.html#a9f6182ae4bcf0e9884551120d9efcef9aea8ec660a4ff1a0cf378ac911be7efb2',1,'scxt::voice::Voice']]]
+  ['effect_7285',['effect',['../structscxt_1_1sfz__support_1_1SFZParser_1_1Header.html#a1d1cfd8ffb84e947f82999c682b666a7a501f9ea46247fec3e513f1a49f2d548a',1,'scxt::sfz_support::SFZParser::Header']]],
+  ['endl_7286',['endL',['../structscxt_1_1ui_1_1app_1_1edit__screen_1_1VariantDisplay.html#a8c72c82f024ad6af2e49918de816504fabf9e9fb3920d2d8a5a87983d9dfa3f94',1,'scxt::ui::app::edit_screen::VariantDisplay']]],
+  ['endp_7287',['endP',['../structscxt_1_1ui_1_1app_1_1edit__screen_1_1VariantDisplay.html#a8c72c82f024ad6af2e49918de816504fadb49e7f174779d056a7d0c4a0ddb40bf',1,'scxt::ui::app::edit_screen::VariantDisplay']]],
+  ['endpoints_7288',['ENDPOINTS',['../structscxt_1_1engine_1_1Zone.html#a102a252ae2c383d786df8136d4af2a48af177f01b411631e4ec5f79071dbfb7e7',1,'scxt::engine::Zone']]],
+  ['engine_5fgroup_7289',['engine_Group',['../structscxt_1_1messaging_1_1audio_1_1AudioToSerialization_1_1ToBeDeleted.html#a5a8b2dc9e45a9ee81a94ef304fb62505ac77866631b1fcd2d68f01b7654d10dd1',1,'scxt::messaging::audio::AudioToSerialization::ToBeDeleted']]],
+  ['engine_5fzone_7290',['engine_Zone',['../structscxt_1_1messaging_1_1audio_1_1AudioToSerialization_1_1ToBeDeleted.html#a5a8b2dc9e45a9ee81a94ef304fb62505abb2e7082ad16486360031ff336659e74',1,'scxt::messaging::audio::AudioToSerialization::ToBeDeleted']]],
+  ['env_7291',['ENV',['../structscxt_1_1modulation_1_1shared_1_1HasModulators.html#a9b03541840ed282f2687903729ec6c08a90a589408899045d76b41b0b3520afaf',1,'scxt::modulation::shared::HasModulators']]],
+  ['error_5fbus_7292',['ERROR_BUS',['../namespacescxt_1_1engine.html#abda9c7d4255cb9369a2cc3208fb3dec8abc5f80341be466fd1044ceb745d8d9f8',1,'scxt::engine']]],
+  ['expecting_5findex_7293',['EXPECTING_INDEX',['../structscxt_1_1ui_1_1connectors_1_1JSONLayoutConsumer.html#a5d74787dedbc4e11c1ab15bf487e61f8a1ccac9958f721a71a57e82cce3f39ec8',1,'scxt::ui::connectors::JSONLayoutConsumer']]],
+  ['expression_7294',['EXPRESSION',['../structscxt_1_1voice_1_1Voice.html#a9f6182ae4bcf0e9884551120d9efcef9aea8ec660a4ff1a0cf378ac911be7efb2',1,'scxt::voice::Voice']]]
 ];
