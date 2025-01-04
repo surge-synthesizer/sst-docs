0beab07aef4a0733034ad507f6278e064c887178
baconpaul <paul@baconpaul.org>
2025-01-04 16:41:45 +0000

    Update doxygen for sst-basic-blocks from github action

--- docs/sst-basic-blocks/search/files_9.js
+++ docs/sst-basic-blocks/search/files_9.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['midside_2eh_0',['MidSide.h',['../MidSide_8h.html',1,'']]],
-  ['modmatrix_2eh_1',['ModMatrix.h',['../ModMatrix_8h.html',1,'']]],
-  ['modmatrixdetails_2eh_2',['ModMatrixDetails.h',['../ModMatrixDetails_8h.html',1,'']]]
+  ['midiccnames_2eh_0',['MIDICCNames.h',['../MIDICCNames_8h.html',1,'']]],
+  ['midside_2eh_1',['MidSide.h',['../MidSide_8h.html',1,'']]],
+  ['modmatrix_2eh_2',['ModMatrix.h',['../ModMatrix_8h.html',1,'']]],
+  ['modmatrixdetails_2eh_3',['ModMatrixDetails.h',['../ModMatrixDetails_8h.html',1,'']]]
 ];
