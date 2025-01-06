c889baf99bebc8de3f679439ea3c1ae7cfddef87
baconpaul <paul@baconpaul.org>
2025-01-06 13:41:14 +0000

    Update doxygen for sst-voicemanager from github action

--- docs/sst-voicemanager/search/variables_c.js
+++ docs/sst-voicemanager/search/variables_c.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['start_0',['START',['../structsst_1_1voicemanager_1_1VoiceInitInstructionsEntry.html#a7bd05fa0213b0f0f0acb5a7c52f0a348',1,'sst::voicemanager::VoiceInitInstructionsEntry']]],
   ['stealingprioritymode_1',['stealingPriorityMode',['../structsst_1_1voicemanager_1_1VoiceManager_1_1Details.html#ab5119895a722c03170323df5ed090a9c',1,'sst::voicemanager::VoiceManager::Details']]],
-  ['sustainon_2',['sustainOn',['../structsst_1_1voicemanager_1_1VoiceManager_1_1Details.html#a6314693e0292248543f2df3cad954767',1,'sst::voicemanager::VoiceManager::Details']]]
+  ['sustainon_2',['sustainOn',['../structsst_1_1voicemanager_1_1VoiceManager_1_1Details.html#a9c9f4ac6c6745187a9e9a3012b0c4547',1,'sst::voicemanager::VoiceManager::Details']]]
 ];
