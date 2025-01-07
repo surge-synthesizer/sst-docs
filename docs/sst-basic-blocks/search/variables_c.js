6aef2b399b79344781dde128064e6b53925b05f3
baconpaul <paul@baconpaul.org>
2025-01-07 00:20:14 +0000

    Update doxygen for sst-basic-blocks from github action

--- docs/sst-basic-blocks/search/variables_c.js
+++ docs/sst-basic-blocks/search/variables_c.js
@@ -2,12 +2,13 @@ var searchData=
 [
   ['n_5funison_0',['n_unison',['../structsst_1_1basic__blocks_1_1dsp_1_1UnisonSetup.html#ac662053d496cf2e8b65ce85a3a194d25',1,'sst::basic_blocks::dsp::UnisonSetup']]],
   ['name_1',['name',['../structsst_1_1basic__blocks_1_1params_1_1ParamMetaData.html#a9b45b3e13bd9167aab02e17e08916231',1,'sst::basic_blocks::params::ParamMetaData']]],
-  ['new_5fv_2',['new_v',['../structsst_1_1basic__blocks_1_1dsp_1_1lipol.html#ac0a74ef8fff3a91ff125b6d1c171eff2',1,'sst::basic_blocks::dsp::lipol']]],
-  ['ninterp_3',['nInterp',['../structsst_1_1basic__blocks_1_1tables_1_1TwoToTheXProvider.html#a92e693b8a412e4be2a53b93143f25a5a',1,'sst::basic_blocks::tables::TwoToTheXProvider']]],
-  ['nlutpoints_4',['nLUTPoints',['../structsst_1_1basic__blocks_1_1modulators_1_1AHDSRShapedSC.html#aad7c9705fbdea8c2b7fe3c3f20ff3b59',1,'sst::basic_blocks::modulators::AHDSRShapedSC']]],
-  ['no_5falternate_5',['NO_ALTERNATE',['../structsst_1_1basic__blocks_1_1params_1_1ParamMetaData.html#a88100c522c6e8305fe3e2be8e3f34371',1,'sst::basic_blocks::params::ParamMetaData']]],
-  ['no_5fquantization_6',['NO_QUANTIZATION',['../structsst_1_1basic__blocks_1_1params_1_1ParamMetaData.html#a33e372a6ffedb317c01faaf6749d3bee',1,'sst::basic_blocks::params::ParamMetaData']]],
-  ['npoints_7',['npoints',['../structsst_1_1basic__blocks_1_1dsp_1_1RunningAverage.html#acb51832fd6e501fbd5f2e99e211eeed2',1,'sst::basic_blocks::dsp::RunningAverage::nPoints'],['../structsst_1_1basic__blocks_1_1tables_1_1DbToLinearProvider.html#a02d87e0bf4ef9d194d817a6ce2f93df1',1,'sst::basic_blocks::tables::DbToLinearProvider::nPoints']]],
-  ['ntables_8',['nTables',['../structsst_1_1basic__blocks_1_1modulators_1_1AHDSRShapedSC.html#aa10d63dd02bc05aacb0758581d870cc5',1,'sst::basic_blocks::modulators::AHDSRShapedSC']]],
-  ['numregisters_9',['numRegisters',['../structsst_1_1basic__blocks_1_1dsp_1_1lipol__sse.html#a871882c9504a3128251bd279b5a7eccd',1,'sst::basic_blocks::dsp::lipol_sse']]]
+  ['needsrandomrestart_2',['needsRandomRestart',['../structsst_1_1basic__blocks_1_1modulators_1_1SimpleLFO.html#a1ce11fb5f6c7659a8b2fbf2c7cadfc4c',1,'sst::basic_blocks::modulators::SimpleLFO']]],
+  ['new_5fv_3',['new_v',['../structsst_1_1basic__blocks_1_1dsp_1_1lipol.html#ac0a74ef8fff3a91ff125b6d1c171eff2',1,'sst::basic_blocks::dsp::lipol']]],
+  ['ninterp_4',['nInterp',['../structsst_1_1basic__blocks_1_1tables_1_1TwoToTheXProvider.html#a92e693b8a412e4be2a53b93143f25a5a',1,'sst::basic_blocks::tables::TwoToTheXProvider']]],
+  ['nlutpoints_5',['nLUTPoints',['../structsst_1_1basic__blocks_1_1modulators_1_1AHDSRShapedSC.html#aad7c9705fbdea8c2b7fe3c3f20ff3b59',1,'sst::basic_blocks::modulators::AHDSRShapedSC']]],
+  ['no_5falternate_6',['NO_ALTERNATE',['../structsst_1_1basic__blocks_1_1params_1_1ParamMetaData.html#a88100c522c6e8305fe3e2be8e3f34371',1,'sst::basic_blocks::params::ParamMetaData']]],
+  ['no_5fquantization_7',['NO_QUANTIZATION',['../structsst_1_1basic__blocks_1_1params_1_1ParamMetaData.html#a33e372a6ffedb317c01faaf6749d3bee',1,'sst::basic_blocks::params::ParamMetaData']]],
+  ['npoints_8',['npoints',['../structsst_1_1basic__blocks_1_1dsp_1_1RunningAverage.html#acb51832fd6e501fbd5f2e99e211eeed2',1,'sst::basic_blocks::dsp::RunningAverage::nPoints'],['../structsst_1_1basic__blocks_1_1tables_1_1DbToLinearProvider.html#a02d87e0bf4ef9d194d817a6ce2f93df1',1,'sst::basic_blocks::tables::DbToLinearProvider::nPoints']]],
+  ['ntables_9',['nTables',['../structsst_1_1basic__blocks_1_1modulators_1_1AHDSRShapedSC.html#aa10d63dd02bc05aacb0758581d870cc5',1,'sst::basic_blocks::modulators::AHDSRShapedSC']]],
+  ['numregisters_10',['numRegisters',['../structsst_1_1basic__blocks_1_1dsp_1_1lipol__sse.html#a871882c9504a3128251bd279b5a7eccd',1,'sst::basic_blocks::dsp::lipol_sse']]]
 ];
