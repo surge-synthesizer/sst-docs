90deba2f34d13a642539febc564dfcfd282e9ddb
baconpaul <paul@baconpaul.org>
2025-01-16 19:30:27 +0000

    Update doxygen for sst-plugininfra from github action

--- docs/sst-plugininfra/search/all_e.js
+++ docs/sst-plugininfra/search/all_e.js
@@ -1,12 +1,13 @@
 var searchData=
 [
-  ['paths_2eh_0',['paths.h',['../paths_8h.html',1,'']]],
-  ['paths_5flinux_2ecpp_1',['paths_linux.cpp',['../paths__linux_8cpp.html',1,'']]],
-  ['paths_5fmacos_2emm_2',['paths_macos.mm',['../paths__macos_8mm.html',1,'']]],
-  ['paths_5fwindows_2ecpp_3',['paths_windows.cpp',['../paths__windows_8cpp.html',1,'']]],
-  ['plugininfra_2eh_4',['plugininfra.h',['../plugininfra_8h.html',1,'']]],
-  ['priors_5',['priorS',['../structsst_1_1plugininfra_1_1cpufeatures_1_1FPUStateGuard.html#aa5b1a1abd29273a5d9467cbd86c63806',1,'sst::plugininfra::cpufeatures::FPUStateGuard']]],
-  ['productname_6',['productName',['../structsst_1_1plugininfra_1_1KeyMapManager.html#aaab8fef2547723538e746e357f989fb7',1,'sst::plugininfra::KeyMapManager']]],
-  ['productpath_7',['productPath',['../structsst_1_1plugininfra_1_1KeyMapManager.html#aa05f53de66faf7de41ff2c6ad5220a81',1,'sst::plugininfra::KeyMapManager']]],
-  ['provider_8',['provider',['../structsst_1_1plugininfra_1_1defaults_1_1Provider.html',1,'Provider&lt; E, maxE &gt;'],['../structsst_1_1plugininfra_1_1defaults_1_1Provider.html#a10fdd448aca4dab2a4cc669b8ec46fd9',1,'sst::plugininfra::defaults::Provider::Provider()']]]
+  ['patch_5fbase_2eh_0',['patch_base.h',['../patch__base_8h.html',1,'']]],
+  ['paths_2eh_1',['paths.h',['../paths_8h.html',1,'']]],
+  ['paths_5flinux_2ecpp_2',['paths_linux.cpp',['../paths__linux_8cpp.html',1,'']]],
+  ['paths_5fmacos_2emm_3',['paths_macos.mm',['../paths__macos_8mm.html',1,'']]],
+  ['paths_5fwindows_2ecpp_4',['paths_windows.cpp',['../paths__windows_8cpp.html',1,'']]],
+  ['plugininfra_2eh_5',['plugininfra.h',['../plugininfra_8h.html',1,'']]],
+  ['priors_6',['priorS',['../structsst_1_1plugininfra_1_1cpufeatures_1_1FPUStateGuard.html#aa5b1a1abd29273a5d9467cbd86c63806',1,'sst::plugininfra::cpufeatures::FPUStateGuard']]],
+  ['productname_7',['productName',['../structsst_1_1plugininfra_1_1KeyMapManager.html#aaab8fef2547723538e746e357f989fb7',1,'sst::plugininfra::KeyMapManager']]],
+  ['productpath_8',['productPath',['../structsst_1_1plugininfra_1_1KeyMapManager.html#aa05f53de66faf7de41ff2c6ad5220a81',1,'sst::plugininfra::KeyMapManager']]],
+  ['provider_9',['provider',['../structsst_1_1plugininfra_1_1defaults_1_1Provider.html',1,'Provider&lt; E, maxE &gt;'],['../structsst_1_1plugininfra_1_1defaults_1_1Provider.html#a10fdd448aca4dab2a4cc669b8ec46fd9',1,'sst::plugininfra::defaults::Provider::Provider()']]]
 ];
