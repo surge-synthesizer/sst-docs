b858e017174305af06d0c8fafa1783fbc5b1c151
baconpaul <paul@baconpaul.org>
2025-01-17 19:38:10 +0000

    Update doxygen for sst-plugininfra from github action

--- docs/sst-plugininfra/search/all_0.js
+++ docs/sst-plugininfra/search/all_0.js
@@ -3,5 +3,6 @@ var searchData=
   ['active_0',['active',['../structsst_1_1plugininfra_1_1KeyMapManager_1_1Binding.html#a03c996f9fcf0e10baeb3e700be0c409a',1,'sst::plugininfra::KeyMapManager::Binding']]],
   ['addbinding_1',['addBinding',['../structsst_1_1plugininfra_1_1KeyMapManager.html#a94a581ca3c759f1ad5e36670fec6247f',1,'sst::plugininfra::KeyMapManager']]],
   ['addoverride_2',['addoverride',['../structsst_1_1plugininfra_1_1defaults_1_1Provider.html#ae303e1831c9e5bc08b343bb1d1d9e238',1,'sst::plugininfra::defaults::Provider::addOverride(E key, const std::string &amp;s)'],['../structsst_1_1plugininfra_1_1defaults_1_1Provider.html#a2fcb90545ce7938772bf2e6d5829cd8f',1,'sst::plugininfra::defaults::Provider::addOverride(E key, int i)']]],
-  ['alt_3',['ALT',['../structsst_1_1plugininfra_1_1KeyMapManager.html#a1be798813a5b7dff8e667e94f175e2d7ab1d948a93e387798ef60b07c24a7c337',1,'sst::plugininfra::KeyMapManager']]]
+  ['allocateconsole_3',['allocateConsole',['../namespacesst_1_1plugininfra_1_1misc__platform.html#a4ab9abe49f909897cec28781fcbfbc25',1,'sst::plugininfra::misc_platform']]],
+  ['alt_4',['ALT',['../structsst_1_1plugininfra_1_1KeyMapManager.html#a1be798813a5b7dff8e667e94f175e2d7ab1d948a93e387798ef60b07c24a7c337',1,'sst::plugininfra::KeyMapManager']]]
 ];
--- docs/sst-plugininfra/search/functions_0.js
+++ docs/sst-plugininfra/search/functions_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['addbinding_0',['addBinding',['../structsst_1_1plugininfra_1_1KeyMapManager.html#a94a581ca3c759f1ad5e36670fec6247f',1,'sst::plugininfra::KeyMapManager']]],
-  ['addoverride_1',['addoverride',['../structsst_1_1plugininfra_1_1defaults_1_1Provider.html#ae303e1831c9e5bc08b343bb1d1d9e238',1,'sst::plugininfra::defaults::Provider::addOverride(E key, const std::string &amp;s)'],['../structsst_1_1plugininfra_1_1defaults_1_1Provider.html#a2fcb90545ce7938772bf2e6d5829cd8f',1,'sst::plugininfra::defaults::Provider::addOverride(E key, int i)']]]
+  ['addoverride_1',['addoverride',['../structsst_1_1plugininfra_1_1defaults_1_1Provider.html#ae303e1831c9e5bc08b343bb1d1d9e238',1,'sst::plugininfra::defaults::Provider::addOverride(E key, const std::string &amp;s)'],['../structsst_1_1plugininfra_1_1defaults_1_1Provider.html#a2fcb90545ce7938772bf2e6d5829cd8f',1,'sst::plugininfra::defaults::Provider::addOverride(E key, int i)']]],
+  ['allocateconsole_2',['allocateConsole',['../namespacesst_1_1plugininfra_1_1misc__platform.html#a4ab9abe49f909897cec28781fcbfbc25',1,'sst::plugininfra::misc_platform']]]
 ];
