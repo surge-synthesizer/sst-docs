0dc5fb9d8d657242dca5aa0bbefd1e3c48d7ff87
baconpaul <paul@baconpaul.org>
2025-01-18 13:57:53 +0000

    Update doxygen for sst-plugininfra from github action

--- docs/sst-plugininfra/search/functions_b.js
+++ docs/sst-plugininfra/search/functions_b.js
@@ -1,9 +1,10 @@
 var searchData=
 [
-  ['patchparamsinfo_0',['patchParamsInfo',['../namespacesst_1_1plugininfra_1_1patch__support.html#a9216ce1e9861a1955dea7f9193cbb2cb',1,'sst::plugininfra::patch_support']]],
-  ['patchparamstexttovalue_1',['patchParamsTextToValue',['../namespacesst_1_1plugininfra_1_1patch__support.html#aba85d94869d73300c252e0607c7fbb73',1,'sst::plugininfra::patch_support']]],
-  ['patchparamsvalue_2',['patchParamsValue',['../namespacesst_1_1plugininfra_1_1patch__support.html#a103c534343eed1dcfaa6725f23d7d336',1,'sst::plugininfra::patch_support']]],
-  ['patchparamsvaluetotext_3',['patchParamsValueToText',['../namespacesst_1_1plugininfra_1_1patch__support.html#aa833d202ea8041394f01ddfa85185a6a',1,'sst::plugininfra::patch_support']]],
-  ['patchtooutstream_4',['patchToOutStream',['../namespacesst_1_1plugininfra_1_1patch__support.html#ac61cf4b214d70347faa56e136b12ddb4',1,'sst::plugininfra::patch_support']]],
-  ['provider_5',['Provider',['../structsst_1_1plugininfra_1_1defaults_1_1Provider.html#a10fdd448aca4dab2a4cc669b8ec46fd9',1,'sst::plugininfra::defaults::Provider']]]
+  ['paramfromclapevent_0',['paramFromClapEvent',['../namespacesst_1_1plugininfra_1_1patch__support.html#a8dfd5d7d6ca669e20909df13b0086b5f',1,'sst::plugininfra::patch_support']]],
+  ['patchparamsinfo_1',['patchParamsInfo',['../namespacesst_1_1plugininfra_1_1patch__support.html#a9216ce1e9861a1955dea7f9193cbb2cb',1,'sst::plugininfra::patch_support']]],
+  ['patchparamstexttovalue_2',['patchParamsTextToValue',['../namespacesst_1_1plugininfra_1_1patch__support.html#aba85d94869d73300c252e0607c7fbb73',1,'sst::plugininfra::patch_support']]],
+  ['patchparamsvalue_3',['patchParamsValue',['../namespacesst_1_1plugininfra_1_1patch__support.html#a103c534343eed1dcfaa6725f23d7d336',1,'sst::plugininfra::patch_support']]],
+  ['patchparamsvaluetotext_4',['patchParamsValueToText',['../namespacesst_1_1plugininfra_1_1patch__support.html#aa833d202ea8041394f01ddfa85185a6a',1,'sst::plugininfra::patch_support']]],
+  ['patchtooutstream_5',['patchToOutStream',['../namespacesst_1_1plugininfra_1_1patch__support.html#ac61cf4b214d70347faa56e136b12ddb4',1,'sst::plugininfra::patch_support']]],
+  ['provider_6',['Provider',['../structsst_1_1plugininfra_1_1defaults_1_1Provider.html#a10fdd448aca4dab2a4cc669b8ec46fd9',1,'sst::plugininfra::defaults::Provider']]]
 ];
