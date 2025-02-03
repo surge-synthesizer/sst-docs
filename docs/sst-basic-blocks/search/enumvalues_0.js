ba30ddc64c54299ed1332f43e86e31ad6d6622ee
baconpaul <paul@baconpaul.org>
2025-02-03 19:19:42 +0000

    Update doxygen for sst-basic-blocks from github action

--- docs/sst-basic-blocks/search/enumvalues_0.js
+++ docs/sst-basic-blocks/search/enumvalues_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['a_5ftwo_5fto_5fthe_5fb_0',['A_TWO_TO_THE_B',['../structsst_1_1basic__blocks_1_1params_1_1ParamMetaData.html#a56667947ffdbdeea280ff228b5e14bd2aacf8bf2155350b1368b6b53a840131e7',1,'sst::basic_blocks::params::ParamMetaData']]],
-  ['additive_1',['ADDITIVE',['../namespacesst_1_1basic__blocks_1_1mod__matrix.html#add86e7c88dd109abea3f708b422f31f0ac0c02294c1fb924d8ee2ae11f9426c0e',1,'sst::basic_blocks::mod_matrix']]]
+  ['additive_1',['ADDITIVE',['../namespacesst_1_1basic__blocks_1_1mod__matrix.html#add86e7c88dd109abea3f708b422f31f0ac0c02294c1fb924d8ee2ae11f9426c0e',1,'sst::basic_blocks::mod_matrix']]],
+  ['allow_5ffractional_5ftypeins_2',['ALLOW_FRACTIONAL_TYPEINS',['../structsst_1_1basic__blocks_1_1params_1_1ParamMetaData.html#a7489d426e232a62a4039ca21e585f487a223d54e4989b11240c5b5e6934cea0d6',1,'sst::basic_blocks::params::ParamMetaData']]]
 ];
