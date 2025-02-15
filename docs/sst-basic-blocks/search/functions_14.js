44475072916686927140e801a055c96b66e51b79
baconpaul <paul@baconpaul.org>
2025-02-15 23:59:42 +0000

    Update doxygen for sst-basic-blocks from github action

--- docs/sst-basic-blocks/search/functions_14.js
+++ docs/sst-basic-blocks/search/functions_14.js
@@ -4,7 +4,8 @@ var searchData=
   ['value_1',['value',['../structsst_1_1basic__blocks_1_1modulators_1_1FXModControl.html#a706067905f223e03202bebf448e28307',1,'sst::basic_blocks::modulators::FXModControl']]],
   ['valueat_2',['valueAt',['../structsst_1_1basic__blocks_1_1dsp_1_1DPWSawOscillator.html#a8a4a4aa5adf7a3fe4bd4e0fcfbbf1fac',1,'sst::basic_blocks::dsp::DPWSawOscillator']]],
   ['valuefromstring_3',['valueFromString',['../structsst_1_1basic__blocks_1_1params_1_1ParamMetaData.html#a7afddaa0202b2bfd4f61e0969757cc9b',1,'sst::basic_blocks::params::ParamMetaData']]],
-  ['valuetoalternatestring_4',['valueToAlternateString',['../structsst_1_1basic__blocks_1_1params_1_1ParamMetaData.html#a98252ceb2bf0cf32c1d076352b43ca7d',1,'sst::basic_blocks::params::ParamMetaData']]],
-  ['valuetostring_5',['valueToString',['../structsst_1_1basic__blocks_1_1params_1_1ParamMetaData.html#ae1d4ee398f822f08c0176cf4b182e1dc',1,'sst::basic_blocks::params::ParamMetaData']]],
-  ['vupeak_6',['VUPeak',['../structsst_1_1basic__blocks_1_1dsp_1_1VUPeak.html#ae8631e2c4419d3c678322b3cf43b874c',1,'sst::basic_blocks::dsp::VUPeak']]]
+  ['valuefromtemposyncnotation_4',['valueFromTemposyncNotation',['../structsst_1_1basic__blocks_1_1params_1_1ParamMetaData.html#ae2c9fe6634b913030c53d8387a35cc69',1,'sst::basic_blocks::params::ParamMetaData']]],
+  ['valuetoalternatestring_5',['valueToAlternateString',['../structsst_1_1basic__blocks_1_1params_1_1ParamMetaData.html#a98252ceb2bf0cf32c1d076352b43ca7d',1,'sst::basic_blocks::params::ParamMetaData']]],
+  ['valuetostring_6',['valueToString',['../structsst_1_1basic__blocks_1_1params_1_1ParamMetaData.html#ae1d4ee398f822f08c0176cf4b182e1dc',1,'sst::basic_blocks::params::ParamMetaData']]],
+  ['vupeak_7',['VUPeak',['../structsst_1_1basic__blocks_1_1dsp_1_1VUPeak.html#ae8631e2c4419d3c678322b3cf43b874c',1,'sst::basic_blocks::dsp::VUPeak']]]
 ];
