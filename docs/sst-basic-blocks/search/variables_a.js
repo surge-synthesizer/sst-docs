b8c64f3be2e5c281b73d4edcf92364ac59b24641
baconpaul <paul@baconpaul.org>
2025-02-17 20:26:59 +0000

    Update doxygen for sst-basic-blocks from github action

--- docs/sst-basic-blocks/search/variables_a.js
+++ docs/sst-basic-blocks/search/variables_a.js
@@ -7,10 +7,12 @@ var searchData=
   ['last_4',['last',['../structsst_1_1basic__blocks_1_1dsp_1_1SlewLimiter.html#adb0d4f54bba03069d82fe732abc4e0c5',1,'sst::basic_blocks::dsp::SlewLimiter']]],
   ['lastdphase_5',['lastdphase',['../structsst_1_1basic__blocks_1_1modulators_1_1AHDSRShapedSC.html#a7c49b3241c910e59657b2659895fc1ba',1,'sst::basic_blocks::modulators::AHDSRShapedSC::lastDPhase'],['../structsst_1_1basic__blocks_1_1modulators_1_1SimpleLFO.html#a7c49b3241c910e59657b2659895fc1ba',1,'sst::basic_blocks::modulators::SimpleLFO::lastDPhase']]],
   ['lastdphasex_6',['lastDPhaseX',['../structsst_1_1basic__blocks_1_1modulators_1_1AHDSRShapedSC.html#a3e498698365a882e638e453839823791',1,'sst::basic_blocks::modulators::AHDSRShapedSC']]],
-  ['lasttarget_7',['lastTarget',['../structsst_1_1basic__blocks_1_1modulators_1_1SimpleLFO.html#a049eb909257a8fedb9c5891ab90d3633',1,'sst::basic_blocks::modulators::SimpleLFO']]],
-  ['linear_8',['LINEAR',['../structsst_1_1basic__blocks_1_1params_1_1ParamMetaData.html#a9255fe1c132b4c49368bf7b130c1dee3',1,'sst::basic_blocks::params::ParamMetaData']]],
-  ['lp_9',['lp',['../structsst_1_1basic__blocks_1_1dsp_1_1OnePoleLag.html#ade395a4e36911661ac6af7d405f00aec',1,'sst::basic_blocks::dsp::OnePoleLag']]],
-  ['lpinv_10',['lpinv',['../structsst_1_1basic__blocks_1_1dsp_1_1OnePoleLag.html#a5924ed462c7da6365576c56d1356f2b3',1,'sst::basic_blocks::dsp::OnePoleLag']]],
-  ['lut_11',['lut',['../structsst_1_1basic__blocks_1_1modulators_1_1AHDSRShapedSC.html#a5d8658bb594ed2015289377203076184',1,'sst::basic_blocks::modulators::AHDSRShapedSC']]],
-  ['lutsinitialized_12',['lutsInitialized',['../structsst_1_1basic__blocks_1_1modulators_1_1AHDSRShapedSC.html#a962b5c3567d97e8dbab13bbcebf90a75',1,'sst::basic_blocks::modulators::AHDSRShapedSC']]]
+  ['lastfrate_7',['lastFRate',['../structsst_1_1basic__blocks_1_1modulators_1_1SimpleLFO.html#afddea5f0355c34fad94fe3b733696405',1,'sst::basic_blocks::modulators::SimpleLFO']]],
+  ['lastrate_8',['lastRate',['../structsst_1_1basic__blocks_1_1modulators_1_1SimpleLFO.html#a73057348cdbdbb316bfe40716fbd5f5f',1,'sst::basic_blocks::modulators::SimpleLFO']]],
+  ['lasttarget_9',['lastTarget',['../structsst_1_1basic__blocks_1_1modulators_1_1SimpleLFO.html#a049eb909257a8fedb9c5891ab90d3633',1,'sst::basic_blocks::modulators::SimpleLFO']]],
+  ['linear_10',['LINEAR',['../structsst_1_1basic__blocks_1_1params_1_1ParamMetaData.html#a9255fe1c132b4c49368bf7b130c1dee3',1,'sst::basic_blocks::params::ParamMetaData']]],
+  ['lp_11',['lp',['../structsst_1_1basic__blocks_1_1dsp_1_1OnePoleLag.html#ade395a4e36911661ac6af7d405f00aec',1,'sst::basic_blocks::dsp::OnePoleLag']]],
+  ['lpinv_12',['lpinv',['../structsst_1_1basic__blocks_1_1dsp_1_1OnePoleLag.html#a5924ed462c7da6365576c56d1356f2b3',1,'sst::basic_blocks::dsp::OnePoleLag']]],
+  ['lut_13',['lut',['../structsst_1_1basic__blocks_1_1modulators_1_1AHDSRShapedSC.html#a5d8658bb594ed2015289377203076184',1,'sst::basic_blocks::modulators::AHDSRShapedSC']]],
+  ['lutsinitialized_14',['lutsInitialized',['../structsst_1_1basic__blocks_1_1modulators_1_1AHDSRShapedSC.html#a962b5c3567d97e8dbab13bbcebf90a75',1,'sst::basic_blocks::modulators::AHDSRShapedSC']]]
 ];
