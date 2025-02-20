f7d94867cf44da47e7c8cf35ed68d9d90771d96a
baconpaul <paul@baconpaul.org>
2025-02-20 23:06:19 +0000

    Update doxygen for sst-cpputils from github action

--- docs/sst-cpputils/search/all_d.js
+++ docs/sst-cpputils/search/all_d.js
@@ -4,8 +4,9 @@ var searchData=
   ['rebind_1',['rebind',['../structsst_1_1cpputils_1_1fixed__memory__allocator__impl_1_1rebind.html',1,'sst::cpputils::fixed_memory_allocator_impl']]],
   ['refcount_5f_2',['refCount_',['../structsst_1_1cpputils_1_1fixed__memory__allocator__impl.html#ad4ae566a3c89516b9a2a63dc51fa24f2',1,'sst::cpputils::fixed_memory_allocator_impl']]],
   ['reference_3',['reference',['../structsst_1_1cpputils_1_1active__set__overlay_1_1iterator.html#a8d4204eb251a857aba99c93b694a0154',1,'sst::cpputils::active_set_overlay::iterator']]],
-  ['removefromactive_4',['removefromactive',['../structsst_1_1cpputils_1_1active__set__overlay.html#ad315a3e088cde3c3915d68632ba6180d',1,'sst::cpputils::active_set_overlay::removeFromActive(T &amp;s)'],['../structsst_1_1cpputils_1_1active__set__overlay.html#a73efe2fd360cd244fd0d57d83191a6a6',1,'sst::cpputils::active_set_overlay::removeFromActive(T *s)']]],
-  ['ring_5fbuffer_2eh_5',['ring_buffer.h',['../ring__buffer_8h.html',1,'']]],
-  ['ringbufferinternal_6',['ringbufferinternal',['../classsst_1_1cpputils_1_1internal_1_1RingBufferInternal.html',1,'RingBufferInternal&lt; N, MemoryOrder &gt;'],['../classsst_1_1cpputils_1_1internal_1_1RingBufferInternal.html#a87ff845c4556e170437df782b086aa42',1,'sst::cpputils::internal::RingBufferInternal::RingBufferInternal()']]],
-  ['ringbufferinternal_3c_20n_2c_20std_3a_3amemory_5forder_5frelaxed_20_3e_7',['RingBufferInternal&lt; N, std::memory_order_relaxed &gt;',['../classsst_1_1cpputils_1_1internal_1_1RingBufferInternal.html',1,'sst::cpputils::internal']]]
+  ['removeall_4',['removeAll',['../structsst_1_1cpputils_1_1active__set__overlay.html#a9b0a5a3ad9972ab0e8eb0b54873aac6b',1,'sst::cpputils::active_set_overlay']]],
+  ['removefromactive_5',['removefromactive',['../structsst_1_1cpputils_1_1active__set__overlay.html#ad315a3e088cde3c3915d68632ba6180d',1,'sst::cpputils::active_set_overlay::removeFromActive(T &amp;s)'],['../structsst_1_1cpputils_1_1active__set__overlay.html#a73efe2fd360cd244fd0d57d83191a6a6',1,'sst::cpputils::active_set_overlay::removeFromActive(T *s)']]],
+  ['ring_5fbuffer_2eh_6',['ring_buffer.h',['../ring__buffer_8h.html',1,'']]],
+  ['ringbufferinternal_7',['ringbufferinternal',['../classsst_1_1cpputils_1_1internal_1_1RingBufferInternal.html',1,'RingBufferInternal&lt; N, MemoryOrder &gt;'],['../classsst_1_1cpputils_1_1internal_1_1RingBufferInternal.html#a87ff845c4556e170437df782b086aa42',1,'sst::cpputils::internal::RingBufferInternal::RingBufferInternal()']]],
+  ['ringbufferinternal_3c_20n_2c_20std_3a_3amemory_5forder_5frelaxed_20_3e_8',['RingBufferInternal&lt; N, std::memory_order_relaxed &gt;',['../classsst_1_1cpputils_1_1internal_1_1RingBufferInternal.html',1,'sst::cpputils::internal']]]
 ];
--- docs/sst-cpputils/search/functions_d.js
+++ docs/sst-cpputils/search/functions_d.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['removefromactive_0',['removefromactive',['../structsst_1_1cpputils_1_1active__set__overlay.html#ad315a3e088cde3c3915d68632ba6180d',1,'sst::cpputils::active_set_overlay::removeFromActive(T &amp;s)'],['../structsst_1_1cpputils_1_1active__set__overlay.html#a73efe2fd360cd244fd0d57d83191a6a6',1,'sst::cpputils::active_set_overlay::removeFromActive(T *s)']]],
-  ['ringbufferinternal_1',['RingBufferInternal',['../classsst_1_1cpputils_1_1internal_1_1RingBufferInternal.html#a87ff845c4556e170437df782b086aa42',1,'sst::cpputils::internal::RingBufferInternal']]]
+  ['removeall_0',['removeAll',['../structsst_1_1cpputils_1_1active__set__overlay.html#a9b0a5a3ad9972ab0e8eb0b54873aac6b',1,'sst::cpputils::active_set_overlay']]],
+  ['removefromactive_1',['removefromactive',['../structsst_1_1cpputils_1_1active__set__overlay.html#ad315a3e088cde3c3915d68632ba6180d',1,'sst::cpputils::active_set_overlay::removeFromActive(T &amp;s)'],['../structsst_1_1cpputils_1_1active__set__overlay.html#a73efe2fd360cd244fd0d57d83191a6a6',1,'sst::cpputils::active_set_overlay::removeFromActive(T *s)']]],
+  ['ringbufferinternal_2',['RingBufferInternal',['../classsst_1_1cpputils_1_1internal_1_1RingBufferInternal.html#a87ff845c4556e170437df782b086aa42',1,'sst::cpputils::internal::RingBufferInternal']]]
 ];
