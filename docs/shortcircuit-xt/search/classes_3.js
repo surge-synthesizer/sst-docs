740f20da2f35a0260faa57851391498cbb23bc1f
baconpaul <paul@baconpaul.org>
2025-04-04 22:04:00 +0000

    Update doxygen for shortcircuit-xt from github action

--- docs/shortcircuit-xt/search/classes_3.js
+++ docs/shortcircuit-xt/search/classes_3.js
@@ -1,18 +1,19 @@
 var searchData=
 [
   ['debugactions_0',['DebugActions',['../structscxt_1_1messaging_1_1client_1_1DebugActions.html',1,'scxt::messaging::client']]],
-  ['defaultsprovider_1',['DefaultsProvider',['../structscxt_1_1infrastructure_1_1DefaultsProvider.html',1,'scxt::infrastructure']]],
-  ['details_2',['Details',['../structscxt_1_1voice_1_1PreviewVoice_1_1Details.html',1,'scxt::voice::PreviewVoice']]],
-  ['devicespane_3',['DevicesPane',['../structscxt_1_1ui_1_1app_1_1browser__ui_1_1DevicesPane.html',1,'scxt::ui::app::browser_ui']]],
-  ['directbooleanpayloaddataattachment_4',['DirectBooleanPayloadDataAttachment',['../structscxt_1_1ui_1_1connectors_1_1DirectBooleanPayloadDataAttachment.html',1,'scxt::ui::connectors']]],
-  ['discretepayloaddataattachment_5',['DiscretePayloadDataAttachment',['../structscxt_1_1ui_1_1connectors_1_1DiscretePayloadDataAttachment.html',1,'scxt::ui::connectors']]],
-  ['discretepayloaddataattachment_3c_20payload_2c_20bool_20_3e_6',['DiscretePayloadDataAttachment&lt; Payload, bool &gt;',['../structscxt_1_1ui_1_1connectors_1_1DiscretePayloadDataAttachment.html',1,'scxt::ui::connectors']]],
-  ['discretepayloaddataattachment_3c_20scxt_3a_3aengine_3a_3azone_3a_3avariants_2c_20bool_20_3e_7',['DiscretePayloadDataAttachment&lt; scxt::engine::Zone::Variants, bool &gt;',['../structscxt_1_1ui_1_1connectors_1_1DiscretePayloadDataAttachment.html',1,'scxt::ui::connectors']]],
-  ['discretepayloaddataattachment_3c_20typename_20gztrait_3a_3arouting_3a_3arouting_2c_20bool_20_3e_8',['DiscretePayloadDataAttachment&lt; typename GZTrait::routing::Routing, bool &gt;',['../structscxt_1_1ui_1_1connectors_1_1DiscretePayloadDataAttachment.html',1,'scxt::ui::connectors']]],
-  ['doublerate_9',['DoubleRate',['../structscxt_1_1modulation_1_1shared_1_1HasModulators_1_1DoubleRate.html',1,'scxt::modulation::shared::HasModulators']]],
-  ['drivearea_10',['DriveArea',['../structscxt_1_1ui_1_1app_1_1browser__ui_1_1DriveArea.html',1,'scxt::ui::app::browser_ui']]],
-  ['drivearearow_11',['DriveAreaRow',['../structscxt_1_1ui_1_1app_1_1browser__ui_1_1DriveArea_1_1DriveAreaRow.html',1,'scxt::ui::app::browser_ui::DriveArea']]],
-  ['drivefsarea_12',['DriveFSArea',['../structscxt_1_1ui_1_1app_1_1browser__ui_1_1DriveFSArea.html',1,'scxt::ui::app::browser_ui']]],
-  ['drivefsrowcomponent_13',['DriveFSRowComponent',['../structscxt_1_1ui_1_1app_1_1browser__ui_1_1DriveFSRowComponent.html',1,'scxt::ui::app::browser_ui']]],
-  ['dummycontinuous_14',['DummyContinuous',['../structscxt_1_1ui_1_1connectors_1_1DummyContinuous.html',1,'scxt::ui::connectors']]]
+  ['debugtimeguard_1',['DebugTimeGuard',['../structscxt_1_1DebugTimeGuard.html',1,'scxt']]],
+  ['defaultsprovider_2',['DefaultsProvider',['../structscxt_1_1infrastructure_1_1DefaultsProvider.html',1,'scxt::infrastructure']]],
+  ['details_3',['Details',['../structscxt_1_1voice_1_1PreviewVoice_1_1Details.html',1,'scxt::voice::PreviewVoice']]],
+  ['devicespane_4',['DevicesPane',['../structscxt_1_1ui_1_1app_1_1browser__ui_1_1DevicesPane.html',1,'scxt::ui::app::browser_ui']]],
+  ['directbooleanpayloaddataattachment_5',['DirectBooleanPayloadDataAttachment',['../structscxt_1_1ui_1_1connectors_1_1DirectBooleanPayloadDataAttachment.html',1,'scxt::ui::connectors']]],
+  ['discretepayloaddataattachment_6',['DiscretePayloadDataAttachment',['../structscxt_1_1ui_1_1connectors_1_1DiscretePayloadDataAttachment.html',1,'scxt::ui::connectors']]],
+  ['discretepayloaddataattachment_3c_20payload_2c_20bool_20_3e_7',['DiscretePayloadDataAttachment&lt; Payload, bool &gt;',['../structscxt_1_1ui_1_1connectors_1_1DiscretePayloadDataAttachment.html',1,'scxt::ui::connectors']]],
+  ['discretepayloaddataattachment_3c_20scxt_3a_3aengine_3a_3azone_3a_3avariants_2c_20bool_20_3e_8',['DiscretePayloadDataAttachment&lt; scxt::engine::Zone::Variants, bool &gt;',['../structscxt_1_1ui_1_1connectors_1_1DiscretePayloadDataAttachment.html',1,'scxt::ui::connectors']]],
+  ['discretepayloaddataattachment_3c_20typename_20gztrait_3a_3arouting_3a_3arouting_2c_20bool_20_3e_9',['DiscretePayloadDataAttachment&lt; typename GZTrait::routing::Routing, bool &gt;',['../structscxt_1_1ui_1_1connectors_1_1DiscretePayloadDataAttachment.html',1,'scxt::ui::connectors']]],
+  ['doublerate_10',['DoubleRate',['../structscxt_1_1modulation_1_1shared_1_1HasModulators_1_1DoubleRate.html',1,'scxt::modulation::shared::HasModulators']]],
+  ['drivearea_11',['DriveArea',['../structscxt_1_1ui_1_1app_1_1browser__ui_1_1DriveArea.html',1,'scxt::ui::app::browser_ui']]],
+  ['drivearearow_12',['DriveAreaRow',['../structscxt_1_1ui_1_1app_1_1browser__ui_1_1DriveArea_1_1DriveAreaRow.html',1,'scxt::ui::app::browser_ui::DriveArea']]],
+  ['drivefsarea_13',['DriveFSArea',['../structscxt_1_1ui_1_1app_1_1browser__ui_1_1DriveFSArea.html',1,'scxt::ui::app::browser_ui']]],
+  ['drivefsrowcomponent_14',['DriveFSRowComponent',['../structscxt_1_1ui_1_1app_1_1browser__ui_1_1DriveFSRowComponent.html',1,'scxt::ui::app::browser_ui']]],
+  ['dummycontinuous_15',['DummyContinuous',['../structscxt_1_1ui_1_1connectors_1_1DummyContinuous.html',1,'scxt::ui::connectors']]]
 ];
