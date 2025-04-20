4e70ad6935b55d96ad330a92d58b35473496ec93
baconpaul <paul@baconpaul.org>
2025-04-20 13:48:50 +0000

    Update doxygen for shortcircuit-xt from github action

--- docs/shortcircuit-xt/search/files_f.js
+++ docs/shortcircuit-xt/search/files_f.js
@@ -3,7 +3,8 @@ var searchData=
   ['random_5fevaluator_2eh_0',['random_evaluator.h',['../random__evaluator_8h.html',1,'']]],
   ['readme_5fjson_5fassets_2emd_1',['README_JSON_ASSETS.md',['../README__JSON__ASSETS_8md.html',1,'']]],
   ['resampling_2eh_2',['resampling.h',['../resampling_8h.html',1,'']]],
-  ['riff_5fmemfile_2eh_3',['riff_memfile.h',['../riff__memfile_8h.html',1,'']]],
-  ['riff_5fwave_2eh_4',['riff_wave.h',['../riff__wave_8h.html',1,'']]],
-  ['routing_2eh_5',['routing.h',['../routing_8h.html',1,'']]]
+  ['riff_2ddump_2ecpp_3',['riff-dump.cpp',['../riff-dump_8cpp.html',1,'']]],
+  ['riff_5fmemfile_2eh_4',['riff_memfile.h',['../riff__memfile_8h.html',1,'']]],
+  ['riff_5fwave_2eh_5',['riff_wave.h',['../riff__wave_8h.html',1,'']]],
+  ['routing_2eh_6',['routing.h',['../routing_8h.html',1,'']]]
 ];
