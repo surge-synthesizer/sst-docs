721667ecd2e45f926c724d22d00835808dc105f4
baconpaul <paul@baconpaul.org>
2025-04-23 16:30:05 +0000

    Update doxygen for shortcircuit-xt from github action

--- docs/shortcircuit-xt/search/classes_a.js
+++ docs/shortcircuit-xt/search/classes_a.js
@@ -5,7 +5,8 @@ var searchData=
   ['kernelop_3c_20interpolationtypes_3a_3asinc_2c_20float_20_3e_2',['KernelOp&lt; InterpolationTypes::Sinc, float &gt;',['../structscxt_1_1dsp_1_1KernelOp_3_01InterpolationTypes_1_1Sinc_00_01float_01_4.html',1,'scxt::dsp']]],
   ['kernelop_3c_20interpolationtypes_3a_3asinc_2c_20int16_5ft_20_3e_3',['KernelOp&lt; InterpolationTypes::Sinc, int16_t &gt;',['../structscxt_1_1dsp_1_1KernelOp_3_01InterpolationTypes_1_1Sinc_00_01int16__t_01_4.html',1,'scxt::dsp']]],
   ['kernelop_3c_20interpolationtypes_3a_3azeroorderhold_2c_20t_20_3e_4',['KernelOp&lt; InterpolationTypes::ZeroOrderHold, T &gt;',['../structscxt_1_1dsp_1_1KernelOp_3_01InterpolationTypes_1_1ZeroOrderHold_00_01T_01_4.html',1,'scxt::dsp']]],
-  ['kernelprocessor_5',['KernelProcessor',['../structscxt_1_1dsp_1_1KernelProcessor.html',1,'scxt::dsp']]],
-  ['keyboardrange_6',['KeyboardRange',['../structscxt_1_1engine_1_1KeyboardRange.html',1,'scxt::engine']]],
-  ['kloc_7',['KLOC',['../structscxt_1_1akai__support_1_1KLOC.html',1,'scxt::akai_support']]]
+  ['kernelop_3c_20interpolationtypes_3a_3azohaa_2c_20t_20_3e_5',['KernelOp&lt; InterpolationTypes::ZOHAA, T &gt;',['../structscxt_1_1dsp_1_1KernelOp_3_01InterpolationTypes_1_1ZOHAA_00_01T_01_4.html',1,'scxt::dsp']]],
+  ['kernelprocessor_6',['KernelProcessor',['../structscxt_1_1dsp_1_1KernelProcessor.html',1,'scxt::dsp']]],
+  ['keyboardrange_7',['KeyboardRange',['../structscxt_1_1engine_1_1KeyboardRange.html',1,'scxt::engine']]],
+  ['kloc_8',['KLOC',['../structscxt_1_1akai__support_1_1KLOC.html',1,'scxt::akai_support']]]
 ];
