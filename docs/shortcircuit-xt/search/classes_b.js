e6d7f310d042971c294e0d8ba3c6a73fcc6a01a7
baconpaul <paul@baconpaul.org>
2025-04-25 12:21:35 +0000

    Update doxygen for shortcircuit-xt from github action

--- docs/shortcircuit-xt/search/classes_b.js
+++ docs/shortcircuit-xt/search/classes_b.js
@@ -4,12 +4,13 @@ var searchData=
   ['lfopane_1',['LfoPane',['../structscxt_1_1ui_1_1app_1_1edit__screen_1_1LfoPane.html',1,'scxt::ui::app::edit_screen']]],
   ['lfosourcebase_2',['LFOSourceBase',['../structscxt_1_1modulation_1_1shared_1_1LFOSourceBase.html',1,'scxt::modulation::shared']]],
   ['lfosourcebase_3c_20scxt_3a_3amodulation_3a_3ashared_3a_3asourceidentifier_2c_20_27grlf_27_2c_20lfospergroup_2c_20registergroupmodsource_20_3e_3',['LFOSourceBase&lt; scxt::modulation::shared::SourceIdentifier, &apos;grlf&apos;, lfosPerGroup, registerGroupModSource &gt;',['../structscxt_1_1modulation_1_1shared_1_1LFOSourceBase.html',1,'scxt::modulation::shared']]],
-  ['lfosourcebase_3c_20scxt_3a_3amodulation_3a_3ashared_3a_3asourceidentifier_2c_20_27znlf_27_2c_20lfosperzone_2c_20registervoicemodsource_20_3e_4',['LFOSourceBase&lt; scxt::modulation::shared::SourceIdentifier, &apos;znlf&apos;, lfosPerZone, registerVoiceModSource &gt;',['../structscxt_1_1modulation_1_1shared_1_1LFOSourceBase.html',1,'scxt::modulation::shared']]],
-  ['lfotarget_5',['lfotarget',['../structscxt_1_1modulation_1_1GroupMatrixEndpoints_1_1LFOTarget.html',1,'GroupMatrixEndpoints::LFOTarget'],['../structscxt_1_1voice_1_1modulation_1_1MatrixEndpoints_1_1LFOTarget.html',1,'MatrixEndpoints::LFOTarget']]],
-  ['lfotargetendpointdata_6',['LFOTargetEndpointData',['../structscxt_1_1modulation_1_1shared_1_1LFOTargetEndpointData.html',1,'scxt::modulation::shared']]],
-  ['lfotargetendpointdata_3c_20tg_2c_20_27glfo_27_3e_7',['LFOTargetEndpointData&lt; TG, &apos;glfo&apos;&gt;',['../structscxt_1_1modulation_1_1shared_1_1LFOTargetEndpointData.html',1,'scxt::modulation::shared']]],
-  ['lfotargetendpointdata_3c_20tg_2c_20_27lfo_20_27_3e_8',['LFOTargetEndpointData&lt; TG, &apos;lfo &apos;&gt;',['../structscxt_1_1modulation_1_1shared_1_1LFOTargetEndpointData.html',1,'scxt::modulation::shared']]],
-  ['loadablefile_9',['LoadableFile',['../structscxt_1_1browser_1_1Browser_1_1LoadableFile.html',1,'scxt::browser::Browser']]],
-  ['lockedexception_10',['LockedException',['../structscxt_1_1browser_1_1SQL_1_1LockedException.html',1,'scxt::browser::SQL']]],
-  ['logscreen_11',['LogScreen',['../structscxt_1_1ui_1_1app_1_1other__screens_1_1LogScreen.html',1,'scxt::ui::app::other_screens']]]
+  ['lfosourcebase_3c_20scxt_3a_3amodulation_3a_3ashared_3a_3asourceidentifier_2c_20_27zglf_27_2c_20lfospergroup_2c_20registervoicemodsource_20_3e_4',['LFOSourceBase&lt; scxt::modulation::shared::SourceIdentifier, &apos;zglf&apos;, lfosPerGroup, registerVoiceModSource &gt;',['../structscxt_1_1modulation_1_1shared_1_1LFOSourceBase.html',1,'scxt::modulation::shared']]],
+  ['lfosourcebase_3c_20scxt_3a_3amodulation_3a_3ashared_3a_3asourceidentifier_2c_20_27znlf_27_2c_20lfosperzone_2c_20registervoicemodsource_20_3e_5',['LFOSourceBase&lt; scxt::modulation::shared::SourceIdentifier, &apos;znlf&apos;, lfosPerZone, registerVoiceModSource &gt;',['../structscxt_1_1modulation_1_1shared_1_1LFOSourceBase.html',1,'scxt::modulation::shared']]],
+  ['lfotarget_6',['lfotarget',['../structscxt_1_1modulation_1_1GroupMatrixEndpoints_1_1LFOTarget.html',1,'GroupMatrixEndpoints::LFOTarget'],['../structscxt_1_1voice_1_1modulation_1_1MatrixEndpoints_1_1LFOTarget.html',1,'MatrixEndpoints::LFOTarget']]],
+  ['lfotargetendpointdata_7',['LFOTargetEndpointData',['../structscxt_1_1modulation_1_1shared_1_1LFOTargetEndpointData.html',1,'scxt::modulation::shared']]],
+  ['lfotargetendpointdata_3c_20tg_2c_20_27glfo_27_3e_8',['LFOTargetEndpointData&lt; TG, &apos;glfo&apos;&gt;',['../structscxt_1_1modulation_1_1shared_1_1LFOTargetEndpointData.html',1,'scxt::modulation::shared']]],
+  ['lfotargetendpointdata_3c_20tg_2c_20_27lfo_20_27_3e_9',['LFOTargetEndpointData&lt; TG, &apos;lfo &apos;&gt;',['../structscxt_1_1modulation_1_1shared_1_1LFOTargetEndpointData.html',1,'scxt::modulation::shared']]],
+  ['loadablefile_10',['LoadableFile',['../structscxt_1_1browser_1_1Browser_1_1LoadableFile.html',1,'scxt::browser::Browser']]],
+  ['lockedexception_11',['LockedException',['../structscxt_1_1browser_1_1SQL_1_1LockedException.html',1,'scxt::browser::SQL']]],
+  ['logscreen_12',['LogScreen',['../structscxt_1_1ui_1_1app_1_1other__screens_1_1LogScreen.html',1,'scxt::ui::app::other_screens']]]
 ];
