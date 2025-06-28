cbe89ca096fc397c0c347d69289c19be26dbe947
baconpaul <paul@baconpaul.org>
2025-06-28 13:16:48 +0000

    Update doxygen for shortcircuit-xt from github action

--- docs/shortcircuit-xt/search/classes_10.js
+++ docs/shortcircuit-xt/search/classes_10.js
@@ -2,16 +2,15 @@ var searchData=
 [
   ['randomevaluator_0',['RandomEvaluator',['../structscxt_1_1modulation_1_1modulators_1_1RandomEvaluator.html',1,'scxt::modulation::modulators']]],
   ['randomstorage_1',['RandomStorage',['../structscxt_1_1modulation_1_1modulators_1_1RandomStorage.html',1,'scxt::modulation::modulators']]],
-  ['rescaler_2',['ReScaler',['../structscxt_1_1ui_1_1app_1_1edit__screen_1_1ModRow_1_1ReScaler.html',1,'scxt::ui::app::edit_screen::ModRow']]],
-  ['riffheader_3',['riffheader',['../structscxt_1_1sample_1_1loaders_1_1riffheader.html',1,'scxt::sample::loaders']]],
-  ['riffmemfile_4',['RIFFMemFile',['../classscxt_1_1sample_1_1loaders_1_1RIFFMemFile.html',1,'scxt::sample::loaders']]],
-  ['riffwavwriter_5',['RIFFWavWriter',['../structscxt_1_1patch__io_1_1riffwav_1_1RIFFWavWriter.html',1,'scxt::patch_io::riffwav']]],
-  ['rngsourcebase_6',['RNGSourceBase',['../structscxt_1_1modulation_1_1shared_1_1RNGSourceBase.html',1,'scxt::modulation::shared']]],
-  ['rngsourcebase_3c_20scxt_3a_3amodulation_3a_3ashared_3a_3asourceidentifier_2c_20_27grng_27_2c_20registergroupmodsource_20_3e_7',['RNGSourceBase&lt; scxt::modulation::shared::SourceIdentifier, &apos;grng&apos;, registerGroupModSource &gt;',['../structscxt_1_1modulation_1_1shared_1_1RNGSourceBase.html',1,'scxt::modulation::shared']]],
-  ['rngsourcebase_3c_20scxt_3a_3amodulation_3a_3ashared_3a_3asourceidentifier_2c_20_27zrng_27_2c_20registervoicemodsource_20_3e_8',['RNGSourceBase&lt; scxt::modulation::shared::SourceIdentifier, &apos;zrng&apos;, registerVoiceModSource &gt;',['../structscxt_1_1modulation_1_1shared_1_1RNGSourceBase.html',1,'scxt::modulation::shared']]],
-  ['routingextrapayload_9',['RoutingExtraPayload',['../structscxt_1_1modulation_1_1shared_1_1RoutingExtraPayload.html',1,'scxt::modulation::shared']]],
-  ['rowaddcomponent_10',['rowAddComponent',['../structscxt_1_1ui_1_1app_1_1edit__screen_1_1GroupZoneSidebarWidget_1_1rowAddComponent.html',1,'scxt::ui::app::edit_screen::GroupZoneSidebarWidget']]],
-  ['rowcomponent_11',['rowComponent',['../structscxt_1_1ui_1_1app_1_1edit__screen_1_1GroupZoneSidebarWidget_1_1rowComponent.html',1,'scxt::ui::app::edit_screen::GroupZoneSidebarWidget']]],
-  ['rowcontents_12',['RowContents',['../structscxt_1_1ui_1_1app_1_1browser__ui_1_1DriveFSArea_1_1RowContents.html',1,'scxt::ui::app::browser_ui::DriveFSArea']]],
-  ['rowtopcomponent_13',['rowTopComponent',['../structscxt_1_1ui_1_1app_1_1edit__screen_1_1GroupZoneSidebarWidget_1_1rowTopComponent.html',1,'scxt::ui::app::edit_screen::GroupZoneSidebarWidget']]]
+  ['riffheader_2',['riffheader',['../structscxt_1_1sample_1_1loaders_1_1riffheader.html',1,'scxt::sample::loaders']]],
+  ['riffmemfile_3',['RIFFMemFile',['../classscxt_1_1sample_1_1loaders_1_1RIFFMemFile.html',1,'scxt::sample::loaders']]],
+  ['riffwavwriter_4',['RIFFWavWriter',['../structscxt_1_1patch__io_1_1riffwav_1_1RIFFWavWriter.html',1,'scxt::patch_io::riffwav']]],
+  ['rngsourcebase_5',['RNGSourceBase',['../structscxt_1_1modulation_1_1shared_1_1RNGSourceBase.html',1,'scxt::modulation::shared']]],
+  ['rngsourcebase_3c_20scxt_3a_3amodulation_3a_3ashared_3a_3asourceidentifier_2c_20_27grng_27_2c_20registergroupmodsource_20_3e_6',['RNGSourceBase&lt; scxt::modulation::shared::SourceIdentifier, &apos;grng&apos;, registerGroupModSource &gt;',['../structscxt_1_1modulation_1_1shared_1_1RNGSourceBase.html',1,'scxt::modulation::shared']]],
+  ['rngsourcebase_3c_20scxt_3a_3amodulation_3a_3ashared_3a_3asourceidentifier_2c_20_27zrng_27_2c_20registervoicemodsource_20_3e_7',['RNGSourceBase&lt; scxt::modulation::shared::SourceIdentifier, &apos;zrng&apos;, registerVoiceModSource &gt;',['../structscxt_1_1modulation_1_1shared_1_1RNGSourceBase.html',1,'scxt::modulation::shared']]],
+  ['routingextrapayload_8',['RoutingExtraPayload',['../structscxt_1_1modulation_1_1shared_1_1RoutingExtraPayload.html',1,'scxt::modulation::shared']]],
+  ['rowaddcomponent_9',['rowAddComponent',['../structscxt_1_1ui_1_1app_1_1edit__screen_1_1GroupZoneSidebarWidget_1_1rowAddComponent.html',1,'scxt::ui::app::edit_screen::GroupZoneSidebarWidget']]],
+  ['rowcomponent_10',['rowComponent',['../structscxt_1_1ui_1_1app_1_1edit__screen_1_1GroupZoneSidebarWidget_1_1rowComponent.html',1,'scxt::ui::app::edit_screen::GroupZoneSidebarWidget']]],
+  ['rowcontents_11',['RowContents',['../structscxt_1_1ui_1_1app_1_1browser__ui_1_1DriveFSArea_1_1RowContents.html',1,'scxt::ui::app::browser_ui::DriveFSArea']]],
+  ['rowtopcomponent_12',['rowTopComponent',['../structscxt_1_1ui_1_1app_1_1edit__screen_1_1GroupZoneSidebarWidget_1_1rowTopComponent.html',1,'scxt::ui::app::edit_screen::GroupZoneSidebarWidget']]]
 ];
