e03ef50f98eb1b6113bb146eec1a6c77e6756f1f
baconpaul <paul@baconpaul.org>
2025-07-19 18:14:50 +0000

    Update doxygen for shortcircuit-xt from github action

--- docs/shortcircuit-xt/search/files_12.js
+++ docs/shortcircuit-xt/search/files_12.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['user_5fdefaults_2eh_0',['user_defaults.h',['../user__defaults_8h.html',1,'']]],
-  ['utils_2ecpp_1',['utils.cpp',['../utils_8cpp.html',1,'']]],
-  ['utils_2eh_2',['utils.h',['../utils_8h.html',1,'']]],
-  ['utils_5ftraits_2eh_3',['utils_traits.h',['../utils__traits_8h.html',1,'']]]
+  ['uihelpers_2eh_0',['UIHelpers.h',['../UIHelpers_8h.html',1,'']]],
+  ['user_5fdefaults_2eh_1',['user_defaults.h',['../user__defaults_8h.html',1,'']]],
+  ['utils_2ecpp_2',['utils.cpp',['../utils_8cpp.html',1,'']]],
+  ['utils_2eh_3',['utils.h',['../utils_8h.html',1,'']]],
+  ['utils_5ftraits_2eh_4',['utils_traits.h',['../utils__traits_8h.html',1,'']]]
 ];
