4aabb0512dcdfedb79307bfd3561d16a697887c4
baconpaul <paul@baconpaul.org>
2025-08-19 21:02:51 +0000

    Update doxygen for sst-basic-blocks from github action

--- docs/sst-basic-blocks/search/classes_4.js
+++ docs/sst-basic-blocks/search/classes_4.js
@@ -1,13 +1,15 @@
 var searchData=
 [
-  ['ebapproxsin_0',['EBApproxSin',['../structsst_1_1basic__blocks_1_1dsp_1_1EBApproxSin.html',1,'sst::basic_blocks::dsp']]],
-  ['eboscillatorbase_1',['EBOscillatorBase',['../structsst_1_1basic__blocks_1_1dsp_1_1EBOscillatorBase.html',1,'sst::basic_blocks::dsp']]],
-  ['eboscillatorbase_3c_20ebapproxsin_3c_20lagsmoothingstrategy_20_3e_2c_20lagsmoothingstrategy_20_3e_2',['EBOscillatorBase&lt; EBApproxSin&lt; LagSmoothingStrategy &gt;, LagSmoothingStrategy &gt;',['../structsst_1_1basic__blocks_1_1dsp_1_1EBOscillatorBase.html',1,'sst::basic_blocks::dsp']]],
-  ['eboscillatorbase_3c_20ebpulse_3c_20lagsmoothingstrategy_20_3e_2c_20lagsmoothingstrategy_20_3e_3',['EBOscillatorBase&lt; EBPulse&lt; LagSmoothingStrategy &gt;, LagSmoothingStrategy &gt;',['../structsst_1_1basic__blocks_1_1dsp_1_1EBOscillatorBase.html',1,'sst::basic_blocks::dsp']]],
-  ['eboscillatorbase_3c_20ebsaw_3c_20lagsmoothingstrategy_20_3e_2c_20lagsmoothingstrategy_20_3e_4',['EBOscillatorBase&lt; EBSaw&lt; LagSmoothingStrategy &gt;, LagSmoothingStrategy &gt;',['../structsst_1_1basic__blocks_1_1dsp_1_1EBOscillatorBase.html',1,'sst::basic_blocks::dsp']]],
-  ['eboscillatorbase_3c_20ebtri_3c_20lagsmoothingstrategy_20_3e_2c_20lagsmoothingstrategy_20_3e_5',['EBOscillatorBase&lt; EBTri&lt; LagSmoothingStrategy &gt;, LagSmoothingStrategy &gt;',['../structsst_1_1basic__blocks_1_1dsp_1_1EBOscillatorBase.html',1,'sst::basic_blocks::dsp']]],
-  ['ebpulse_6',['EBPulse',['../structsst_1_1basic__blocks_1_1dsp_1_1EBPulse.html',1,'sst::basic_blocks::dsp']]],
-  ['ebsaw_7',['EBSaw',['../structsst_1_1basic__blocks_1_1dsp_1_1EBSaw.html',1,'sst::basic_blocks::dsp']]],
-  ['ebtri_8',['EBTri',['../structsst_1_1basic__blocks_1_1dsp_1_1EBTri.html',1,'sst::basic_blocks::dsp']]],
-  ['equaltuningprovider_9',['EqualTuningProvider',['../structsst_1_1basic__blocks_1_1tables_1_1EqualTuningProvider.html',1,'sst::basic_blocks::tables']]]
+  ['ebapproxsemisin_0',['EBApproxSemiSin',['../structsst_1_1basic__blocks_1_1dsp_1_1EBApproxSemiSin.html',1,'sst::basic_blocks::dsp']]],
+  ['ebapproxsin_1',['EBApproxSin',['../structsst_1_1basic__blocks_1_1dsp_1_1EBApproxSin.html',1,'sst::basic_blocks::dsp']]],
+  ['eboscillatorbase_2',['EBOscillatorBase',['../structsst_1_1basic__blocks_1_1dsp_1_1EBOscillatorBase.html',1,'sst::basic_blocks::dsp']]],
+  ['eboscillatorbase_3c_20ebapproxsemisin_3c_20lagsmoothingstrategy_20_3e_2c_20lagsmoothingstrategy_20_3e_3',['EBOscillatorBase&lt; EBApproxSemiSin&lt; LagSmoothingStrategy &gt;, LagSmoothingStrategy &gt;',['../structsst_1_1basic__blocks_1_1dsp_1_1EBOscillatorBase.html',1,'sst::basic_blocks::dsp']]],
+  ['eboscillatorbase_3c_20ebapproxsin_3c_20lagsmoothingstrategy_20_3e_2c_20lagsmoothingstrategy_20_3e_4',['EBOscillatorBase&lt; EBApproxSin&lt; LagSmoothingStrategy &gt;, LagSmoothingStrategy &gt;',['../structsst_1_1basic__blocks_1_1dsp_1_1EBOscillatorBase.html',1,'sst::basic_blocks::dsp']]],
+  ['eboscillatorbase_3c_20ebpulse_3c_20lagsmoothingstrategy_20_3e_2c_20lagsmoothingstrategy_20_3e_5',['EBOscillatorBase&lt; EBPulse&lt; LagSmoothingStrategy &gt;, LagSmoothingStrategy &gt;',['../structsst_1_1basic__blocks_1_1dsp_1_1EBOscillatorBase.html',1,'sst::basic_blocks::dsp']]],
+  ['eboscillatorbase_3c_20ebsaw_3c_20lagsmoothingstrategy_20_3e_2c_20lagsmoothingstrategy_20_3e_6',['EBOscillatorBase&lt; EBSaw&lt; LagSmoothingStrategy &gt;, LagSmoothingStrategy &gt;',['../structsst_1_1basic__blocks_1_1dsp_1_1EBOscillatorBase.html',1,'sst::basic_blocks::dsp']]],
+  ['eboscillatorbase_3c_20ebtri_3c_20lagsmoothingstrategy_20_3e_2c_20lagsmoothingstrategy_20_3e_7',['EBOscillatorBase&lt; EBTri&lt; LagSmoothingStrategy &gt;, LagSmoothingStrategy &gt;',['../structsst_1_1basic__blocks_1_1dsp_1_1EBOscillatorBase.html',1,'sst::basic_blocks::dsp']]],
+  ['ebpulse_8',['EBPulse',['../structsst_1_1basic__blocks_1_1dsp_1_1EBPulse.html',1,'sst::basic_blocks::dsp']]],
+  ['ebsaw_9',['EBSaw',['../structsst_1_1basic__blocks_1_1dsp_1_1EBSaw.html',1,'sst::basic_blocks::dsp']]],
+  ['ebtri_10',['EBTri',['../structsst_1_1basic__blocks_1_1dsp_1_1EBTri.html',1,'sst::basic_blocks::dsp']]],
+  ['equaltuningprovider_11',['EqualTuningProvider',['../structsst_1_1basic__blocks_1_1tables_1_1EqualTuningProvider.html',1,'sst::basic_blocks::tables']]]
 ];
