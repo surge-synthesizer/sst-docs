39a247b4169307117205b210934fcaddbeb95372
baconpaul <paul@baconpaul.org>
2025-08-20 15:55:58 +0000

    Update doxygen for sst-basic-blocks from github action

--- docs/sst-basic-blocks/search/functions_2.js
+++ docs/sst-basic-blocks/search/functions_2.js
@@ -8,5 +8,6 @@ var searchData=
   ['copy_5ffrom_5fto_5',['copy_from_to',['../namespacesst_1_1basic__blocks_1_1mechanics.html#a93b9a6d9d2475305e9120051b44dc9c0',1,'sst::basic_blocks::mechanics']]],
   ['correlated_5fnoise_5fo2mk2_5fsupplied_5fvalue_6',['correlated_noise_o2mk2_supplied_value',['../namespacesst_1_1basic__blocks_1_1dsp.html#ad431caeec17d8a36b30bb9921a312cae',1,'sst::basic_blocks::dsp']]],
   ['correlated_5fnoise_5fo2mk2_5fsuppliedrng_7',['correlated_noise_o2mk2_suppliedrng',['../namespacesst_1_1basic__blocks_1_1dsp.html#a3f60d46ce011d4d89d74d2c9f9c36e2a',1,'sst::basic_blocks::dsp']]],
-  ['cubic_5fipol_8',['cubic_ipol',['../namespacesst_1_1basic__blocks_1_1dsp.html#aba363ecc65489af3901abcf0954c48fd',1,'sst::basic_blocks::dsp']]]
+  ['csip_8',['csip',['../structsst_1_1basic__blocks_1_1dsp_1_1EBOscillatorBase.html#acd55b08c52e0a32d07cd8d7b8e800ed9',1,'sst::basic_blocks::dsp::EBOscillatorBase']]],
+  ['cubic_5fipol_9',['cubic_ipol',['../namespacesst_1_1basic__blocks_1_1dsp.html#aba363ecc65489af3901abcf0954c48fd',1,'sst::basic_blocks::dsp']]]
 ];
