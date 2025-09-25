fbca8442aa0d03a5fe1b9895935a3ba65d0741e2
baconpaul <paul@baconpaul.org>
2025-09-25 13:32:50 +0000

    Update doxygen for sst-plugininfra from github action

--- docs/sst-plugininfra/search/all_f.js
+++ docs/sst-plugininfra/search/all_f.js
@@ -9,6 +9,7 @@ var searchData=
   ['sst_3a_3aplugininfra_3a_3amisc_5fplatform_6',['misc_platform',['../namespacesst_1_1plugininfra_1_1misc__platform.html',1,'sst::plugininfra']]],
   ['sst_3a_3aplugininfra_3a_3apatch_5fsupport_7',['patch_support',['../namespacesst_1_1plugininfra_1_1patch__support.html',1,'sst::plugininfra']]],
   ['sst_3a_3aplugininfra_3a_3apaths_8',['paths',['../namespacesst_1_1plugininfra_1_1paths.html',1,'sst::plugininfra']]],
-  ['streamtoxml_9',['streamToXML',['../structsst_1_1plugininfra_1_1KeyMapManager.html#a24abfe7b30ea0535de9b8e75df4b7f11',1,'sst::plugininfra::KeyMapManager']]],
-  ['stringtoenum_10',['stringToEnum',['../structsst_1_1plugininfra_1_1KeyMapManager.html#a3ea05f0824b2827f3c58ecd76391e1bd',1,'sst::plugininfra::KeyMapManager']]]
+  ['stacktracetostring_9',['stackTraceToString',['../namespacesst_1_1plugininfra_1_1misc__platform.html#af7413b8ebe9d6ecd40f7c42eaaa57860',1,'sst::plugininfra::misc_platform']]],
+  ['streamtoxml_10',['streamToXML',['../structsst_1_1plugininfra_1_1KeyMapManager.html#a24abfe7b30ea0535de9b8e75df4b7f11',1,'sst::plugininfra::KeyMapManager']]],
+  ['stringtoenum_11',['stringToEnum',['../structsst_1_1plugininfra_1_1KeyMapManager.html#a3ea05f0824b2827f3c58ecd76391e1bd',1,'sst::plugininfra::KeyMapManager']]]
 ];
--- docs/sst-plugininfra/search/functions_c.js
+++ docs/sst-plugininfra/search/functions_c.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['sharedlibrarybinarypath_0',['sharedLibraryBinaryPath',['../namespacesst_1_1plugininfra_1_1paths.html#aad28409b8978837a804ebd52aee4b119',1,'sst::plugininfra::paths']]],
-  ['streamtoxml_1',['streamToXML',['../structsst_1_1plugininfra_1_1KeyMapManager.html#a24abfe7b30ea0535de9b8e75df4b7f11',1,'sst::plugininfra::KeyMapManager']]]
+  ['stacktracetostring_1',['stackTraceToString',['../namespacesst_1_1plugininfra_1_1misc__platform.html#af7413b8ebe9d6ecd40f7c42eaaa57860',1,'sst::plugininfra::misc_platform']]],
+  ['streamtoxml_2',['streamToXML',['../structsst_1_1plugininfra_1_1KeyMapManager.html#a24abfe7b30ea0535de9b8e75df4b7f11',1,'sst::plugininfra::KeyMapManager']]]
 ];
