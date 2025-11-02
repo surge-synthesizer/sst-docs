62d18788e8d58cf79c68aacb8b9a00c8131d3e56
baconpaul <paul@baconpaul.org>
2025-11-02 20:20:19 +0000

    Update doxygen for shortcircuit-xt from github action

--- docs/shortcircuit-xt/search/all_0.js
+++ docs/shortcircuit-xt/search/all_0.js
@@ -220,7 +220,7 @@ var searchData=
   ['auxattachments_217',['auxAttachments',['../structscxt_1_1ui_1_1app_1_1mixer__screen_1_1ChannelStrip.html#a5c173d6bb580abb6f7ba6914a123ce70',1,'scxt::ui::app::mixer_screen::ChannelStrip']]],
   ['auxbusses_218',['auxBusses',['../structscxt_1_1engine_1_1Patch_1_1Busses.html#a46ef26c886fc445b54d3c4c071ff900d',1,'scxt::engine::Patch::Busses']]],
   ['auxlabel_219',['auxLabel',['../structscxt_1_1ui_1_1app_1_1mixer__screen_1_1ChannelStrip.html#a62286cab4aa2369c58e0921c8fdc07c3',1,'scxt::ui::app::mixer_screen::ChannelStrip']]],
-  ['auxlocation_220',['auxlocation',['../structscxt_1_1engine_1_1Bus_1_1BusSendStorage.html#a2a79863b610e64afc88282f0dee0f411',1,'scxt::engine::Bus::BusSendStorage::AuxLocation'],['../structscxt_1_1engine_1_1Bus_1_1BusSendStorage.html#ab26241814dbcea6b2a648b59e71798e4',1,'scxt::engine::Bus::BusSendStorage::auxLocation']]],
+  ['auxlocation_220',['auxlocation',['../structscxt_1_1engine_1_1Bus_1_1BusSendStorage.html#ab26241814dbcea6b2a648b59e71798e4',1,'scxt::engine::Bus::BusSendStorage::auxLocation'],['../structscxt_1_1engine_1_1Bus_1_1BusSendStorage.html#a2a79863b610e64afc88282f0dee0f411',1,'scxt::engine::Bus::BusSendStorage::AuxLocation']]],
   ['auxoutputpostvca_221',['auxoutputPostVCA',['../structscxt_1_1engine_1_1Bus.html#a0aae9dc175e62a8887f90bad062a64ec',1,'scxt::engine::Bus']]],
   ['auxoutputprefx_222',['auxoutputPreFX',['../structscxt_1_1engine_1_1Bus.html#a9d6b1733629e9d4255b6a79b6bc454d2',1,'scxt::engine::Bus']]],
   ['auxoutputprevca_223',['auxoutputPreVCA',['../structscxt_1_1engine_1_1Bus.html#aa0a981f8c66c55403f4983074660a43b',1,'scxt::engine::Bus']]],
--- docs/shortcircuit-xt/search/variables_9.js
+++ docs/shortcircuit-xt/search/variables_9.js
@@ -1,8 +1,9 @@
 var searchData=
 [
   ['jog_0',['jog',['../structscxt_1_1ui_1_1app_1_1edit__screen_1_1StepLFOPane.html#ae041af4417d7f4e4d669f2920268c82f',1,'scxt::ui::app::edit_screen::StepLFOPane']]],
-  ['jsondeacteditors_1',['jsonDeactEditors',['../structscxt_1_1ui_1_1app_1_1edit__screen_1_1ProcessorPane.html#acb103c46ae32b80f6b5a290eb3cf2b25',1,'scxt::ui::app::edit_screen::ProcessorPane']]],
-  ['jsonfloateditors_2',['jsonFloatEditors',['../structscxt_1_1ui_1_1app_1_1edit__screen_1_1ProcessorPane.html#a535c6ba50967a4e79ce90e668ac23187',1,'scxt::ui::app::edit_screen::ProcessorPane']]],
-  ['jsoninteditors_3',['jsonIntEditors',['../structscxt_1_1ui_1_1app_1_1edit__screen_1_1ProcessorPane.html#a54aae99ddd35d0eabc974e0e7acada53',1,'scxt::ui::app::edit_screen::ProcessorPane']]],
-  ['jsonlabels_4',['jsonlabels',['../structscxt_1_1ui_1_1app_1_1edit__screen_1_1ProcessorPane.html#ad1b58857db4b94ac289c8d9b41467430',1,'scxt::ui::app::edit_screen::ProcessorPane::jsonLabels'],['../structscxt_1_1ui_1_1app_1_1shared_1_1PartEffectsPane.html#ad1b58857db4b94ac289c8d9b41467430',1,'scxt::ui::app::shared::PartEffectsPane::jsonLabels']]]
+  ['jsoncomponents_1',['jsonComponents',['../structscxt_1_1ui_1_1app_1_1edit__screen_1_1ProcTab.html#a2bab45f4503c1beee61d67eb2a86a9b1',1,'scxt::ui::app::edit_screen::ProcTab']]],
+  ['jsondeacteditors_2',['jsonDeactEditors',['../structscxt_1_1ui_1_1app_1_1edit__screen_1_1ProcessorPane.html#acb103c46ae32b80f6b5a290eb3cf2b25',1,'scxt::ui::app::edit_screen::ProcessorPane']]],
+  ['jsonfloateditors_3',['jsonFloatEditors',['../structscxt_1_1ui_1_1app_1_1edit__screen_1_1ProcessorPane.html#a535c6ba50967a4e79ce90e668ac23187',1,'scxt::ui::app::edit_screen::ProcessorPane']]],
+  ['jsoninteditors_4',['jsonIntEditors',['../structscxt_1_1ui_1_1app_1_1edit__screen_1_1ProcessorPane.html#a54aae99ddd35d0eabc974e0e7acada53',1,'scxt::ui::app::edit_screen::ProcessorPane']]],
+  ['jsonlabels_5',['jsonlabels',['../structscxt_1_1ui_1_1app_1_1edit__screen_1_1ProcessorPane.html#ad1b58857db4b94ac289c8d9b41467430',1,'scxt::ui::app::edit_screen::ProcessorPane::jsonLabels'],['../structscxt_1_1ui_1_1app_1_1shared_1_1PartEffectsPane.html#ad1b58857db4b94ac289c8d9b41467430',1,'scxt::ui::app::shared::PartEffectsPane::jsonLabels']]]
 ];
