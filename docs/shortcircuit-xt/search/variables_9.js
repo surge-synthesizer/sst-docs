1233f0c321b31219003a34df6e5a8d6c87e3bb78
baconpaul <paul@baconpaul.org>
2025-11-05 22:19:28 +0000

    Update doxygen for shortcircuit-xt from github action

--- docs/shortcircuit-xt/search/variables_9.js
+++ docs/shortcircuit-xt/search/variables_9.js
@@ -3,7 +3,8 @@ var searchData=
   ['jog_0',['jog',['../structscxt_1_1ui_1_1app_1_1edit__screen_1_1StepLFOPane.html#ae041af4417d7f4e4d669f2920268c82f',1,'scxt::ui::app::edit_screen::StepLFOPane']]],
   ['jsoncomponents_1',['jsonComponents',['../structscxt_1_1ui_1_1app_1_1edit__screen_1_1ProcTab.html#a2bab45f4503c1beee61d67eb2a86a9b1',1,'scxt::ui::app::edit_screen::ProcTab']]],
   ['jsondeacteditors_2',['jsonDeactEditors',['../structscxt_1_1ui_1_1app_1_1edit__screen_1_1ProcessorPane.html#acb103c46ae32b80f6b5a290eb3cf2b25',1,'scxt::ui::app::edit_screen::ProcessorPane']]],
-  ['jsonfloateditors_3',['jsonFloatEditors',['../structscxt_1_1ui_1_1app_1_1edit__screen_1_1ProcessorPane.html#a535c6ba50967a4e79ce90e668ac23187',1,'scxt::ui::app::edit_screen::ProcessorPane']]],
-  ['jsoninteditors_4',['jsonIntEditors',['../structscxt_1_1ui_1_1app_1_1edit__screen_1_1ProcessorPane.html#a54aae99ddd35d0eabc974e0e7acada53',1,'scxt::ui::app::edit_screen::ProcessorPane']]],
-  ['jsonlabels_5',['jsonlabels',['../structscxt_1_1ui_1_1app_1_1edit__screen_1_1ProcessorPane.html#ad1b58857db4b94ac289c8d9b41467430',1,'scxt::ui::app::edit_screen::ProcessorPane::jsonLabels'],['../structscxt_1_1ui_1_1app_1_1shared_1_1PartEffectsPane.html#ad1b58857db4b94ac289c8d9b41467430',1,'scxt::ui::app::shared::PartEffectsPane::jsonLabels']]]
+  ['jsondefinitions_3',['jsonDefinitions',['../structscxt_1_1ui_1_1app_1_1edit__screen_1_1ProcessorPane.html#af880f91209777fca16e58bc432a53db0',1,'scxt::ui::app::edit_screen::ProcessorPane']]],
+  ['jsonfloateditors_4',['jsonFloatEditors',['../structscxt_1_1ui_1_1app_1_1edit__screen_1_1ProcessorPane.html#a535c6ba50967a4e79ce90e668ac23187',1,'scxt::ui::app::edit_screen::ProcessorPane']]],
+  ['jsoninteditors_5',['jsonIntEditors',['../structscxt_1_1ui_1_1app_1_1edit__screen_1_1ProcessorPane.html#a54aae99ddd35d0eabc974e0e7acada53',1,'scxt::ui::app::edit_screen::ProcessorPane']]],
+  ['jsonlabels_6',['jsonlabels',['../structscxt_1_1ui_1_1app_1_1edit__screen_1_1ProcessorPane.html#ad1b58857db4b94ac289c8d9b41467430',1,'scxt::ui::app::edit_screen::ProcessorPane::jsonLabels'],['../structscxt_1_1ui_1_1app_1_1shared_1_1PartEffectsPane.html#ad1b58857db4b94ac289c8d9b41467430',1,'scxt::ui::app::shared::PartEffectsPane::jsonLabels']]]
 ];
