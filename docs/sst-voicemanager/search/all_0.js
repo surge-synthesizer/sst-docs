ee9d927b06740198e01a93eaaa93f1ef32dd1fdf
baconpaul <paul@baconpaul.org>
2025-11-07 15:17:36 +0000

    Update doxygen for sst-voicemanager from github action

--- docs/sst-voicemanager/search/all_0.js
+++ docs/sst-voicemanager/search/all_0.js
@@ -3,6 +3,7 @@ var searchData=
   ['activevoicecookie_0',['activeVoiceCookie',['../structsst_1_1voicemanager_1_1VoiceManager_1_1Details_1_1VoiceInfo.html#a306e725e5cce35aa3cd6b18ea30e311d',1,'sst::voicemanager::VoiceManager::Details::VoiceInfo']]],
   ['allnotesoff_1',['allNotesOff',['../structsst_1_1voicemanager_1_1VoiceManager.html#aec246739838d962211ec3124c7f26ff3',1,'sst::voicemanager::VoiceManager']]],
   ['allsoundsoff_2',['allSoundsOff',['../structsst_1_1voicemanager_1_1VoiceManager.html#a0f2ae1a5aceaf3cd3ee076e8769e31f3',1,'sst::voicemanager::VoiceManager']]],
-  ['anykeyheldfor_3',['anyKeyHeldFor',['../structsst_1_1voicemanager_1_1VoiceManager_1_1Details.html#a6d06d007744cea6ad8cf33af46789dbb',1,'sst::voicemanager::VoiceManager::Details']]],
-  ['applymidi1message_4',['applyMidi1Message',['../namespacesst_1_1voicemanager.html#a32312f0f1a31f1d6009c95139257f8f6',1,'sst::voicemanager']]]
+  ['allsoundsoffmatching_3',['allSoundsOffMatching',['../structsst_1_1voicemanager_1_1VoiceManager.html#a6da87eca8370ce17dcab88928ddcc2ac',1,'sst::voicemanager::VoiceManager']]],
+  ['anykeyheldfor_4',['anyKeyHeldFor',['../structsst_1_1voicemanager_1_1VoiceManager_1_1Details.html#a6d06d007744cea6ad8cf33af46789dbb',1,'sst::voicemanager::VoiceManager::Details']]],
+  ['applymidi1message_5',['applyMidi1Message',['../namespacesst_1_1voicemanager.html#a32312f0f1a31f1d6009c95139257f8f6',1,'sst::voicemanager']]]
 ];
--- docs/sst-voicemanager/search/functions_0.js
+++ docs/sst-voicemanager/search/functions_0.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['allnotesoff_0',['allNotesOff',['../structsst_1_1voicemanager_1_1VoiceManager.html#aec246739838d962211ec3124c7f26ff3',1,'sst::voicemanager::VoiceManager']]],
   ['allsoundsoff_1',['allSoundsOff',['../structsst_1_1voicemanager_1_1VoiceManager.html#a0f2ae1a5aceaf3cd3ee076e8769e31f3',1,'sst::voicemanager::VoiceManager']]],
-  ['anykeyheldfor_2',['anyKeyHeldFor',['../structsst_1_1voicemanager_1_1VoiceManager_1_1Details.html#a6d06d007744cea6ad8cf33af46789dbb',1,'sst::voicemanager::VoiceManager::Details']]],
-  ['applymidi1message_3',['applyMidi1Message',['../namespacesst_1_1voicemanager.html#a32312f0f1a31f1d6009c95139257f8f6',1,'sst::voicemanager']]]
+  ['allsoundsoffmatching_2',['allSoundsOffMatching',['../structsst_1_1voicemanager_1_1VoiceManager.html#a6da87eca8370ce17dcab88928ddcc2ac',1,'sst::voicemanager::VoiceManager']]],
+  ['anykeyheldfor_3',['anyKeyHeldFor',['../structsst_1_1voicemanager_1_1VoiceManager_1_1Details.html#a6d06d007744cea6ad8cf33af46789dbb',1,'sst::voicemanager::VoiceManager::Details']]],
+  ['applymidi1message_4',['applyMidi1Message',['../namespacesst_1_1voicemanager.html#a32312f0f1a31f1d6009c95139257f8f6',1,'sst::voicemanager']]]
 ];
