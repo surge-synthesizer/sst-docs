5cc2b17529ab7e39a86c106d1a2de9bfaf476f29
baconpaul <paul@baconpaul.org>
2025-11-10 23:40:34 +0000

    Update doxygen for shortcircuit-xt from github action

--- docs/shortcircuit-xt/search/all_0.js
+++ docs/shortcircuit-xt/search/all_0.js
@@ -221,7 +221,7 @@ var searchData=
   ['auxattachments_218',['auxAttachments',['../structscxt_1_1ui_1_1app_1_1mixer__screen_1_1ChannelStrip.html#a5c173d6bb580abb6f7ba6914a123ce70',1,'scxt::ui::app::mixer_screen::ChannelStrip']]],
   ['auxbusses_219',['auxBusses',['../structscxt_1_1engine_1_1Patch_1_1Busses.html#a46ef26c886fc445b54d3c4c071ff900d',1,'scxt::engine::Patch::Busses']]],
   ['auxlabel_220',['auxLabel',['../structscxt_1_1ui_1_1app_1_1mixer__screen_1_1ChannelStrip.html#a62286cab4aa2369c58e0921c8fdc07c3',1,'scxt::ui::app::mixer_screen::ChannelStrip']]],
-  ['auxlocation_221',['auxlocation',['../structscxt_1_1engine_1_1Bus_1_1BusSendStorage.html#ab26241814dbcea6b2a648b59e71798e4',1,'scxt::engine::Bus::BusSendStorage::auxLocation'],['../structscxt_1_1engine_1_1Bus_1_1BusSendStorage.html#a2a79863b610e64afc88282f0dee0f411',1,'scxt::engine::Bus::BusSendStorage::AuxLocation']]],
+  ['auxlocation_221',['auxlocation',['../structscxt_1_1engine_1_1Bus_1_1BusSendStorage.html#a2a79863b610e64afc88282f0dee0f411',1,'scxt::engine::Bus::BusSendStorage::AuxLocation'],['../structscxt_1_1engine_1_1Bus_1_1BusSendStorage.html#ab26241814dbcea6b2a648b59e71798e4',1,'scxt::engine::Bus::BusSendStorage::auxLocation']]],
   ['auxoutputpostvca_222',['auxoutputPostVCA',['../structscxt_1_1engine_1_1Bus.html#a0aae9dc175e62a8887f90bad062a64ec',1,'scxt::engine::Bus']]],
   ['auxoutputprefx_223',['auxoutputPreFX',['../structscxt_1_1engine_1_1Bus.html#a9d6b1733629e9d4255b6a79b6bc454d2',1,'scxt::engine::Bus']]],
   ['auxoutputprevca_224',['auxoutputPreVCA',['../structscxt_1_1engine_1_1Bus.html#aa0a981f8c66c55403f4983074660a43b',1,'scxt::engine::Bus']]],
