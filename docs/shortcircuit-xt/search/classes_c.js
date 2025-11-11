186da41e71d0e8d2923e2111d95fadadd1bf0e0c
baconpaul <paul@baconpaul.org>
2025-11-11 02:45:39 +0000

    Update doxygen for shortcircuit-xt from github action

--- docs/shortcircuit-xt/search/classes_c.js
+++ docs/shortcircuit-xt/search/classes_c.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['narrowverticalmenu_0',['NarrowVerticalMenu',['../structscxt_1_1ui_1_1app_1_1shared_1_1NarrowVerticalMenu.html',1,'scxt::ui::app::shared']]],
   ['noselection_1',['NoSelection',['../structscxt_1_1ui_1_1app_1_1edit__screen_1_1NoSelection.html',1,'scxt::ui::app::edit_screen']]],
-  ['noteexpressionsources_2',['NoteExpressionSources',['../structscxt_1_1voice_1_1modulation_1_1MatrixEndpoints_1_1Sources_1_1NoteExpressionSources.html',1,'scxt::voice::modulation::MatrixEndpoints::Sources']]]
+  ['noteexpressionsources_2',['NoteExpressionSources',['../structscxt_1_1voice_1_1modulation_1_1MatrixEndpoints_1_1Sources_1_1NoteExpressionSources.html',1,'scxt::voice::modulation::MatrixEndpoints::Sources']]],
+  ['numdenquantatt_3',['NumDenQuantAtt',['../structscxt_1_1ui_1_1app_1_1edit__screen_1_1MiscPanel_1_1NumDenQuantAtt.html',1,'scxt::ui::app::edit_screen::MiscPanel']]]
 ];
