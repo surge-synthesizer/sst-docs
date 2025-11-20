22a14acdb0a0b98f23f63c9d8843981f0aa5449c
baconpaul <paul@baconpaul.org>
2025-11-20 01:56:17 +0000

    Update doxygen for shortcircuit-xt from github action

--- docs/shortcircuit-xt/search/functions_16.js
+++ docs/shortcircuit-xt/search/functions_16.js
@@ -3,7 +3,7 @@ var searchData=
   ['waitforjobsoutstandingcomplete_0',['waitForJobsOutstandingComplete',['../structscxt_1_1browser_1_1BrowserDB.html#a4dcf028d138751f68edd2fee9197982a',1,'scxt::browser::BrowserDB']]],
   ['welcomescreen_1',['WelcomeScreen',['../structscxt_1_1ui_1_1app_1_1other__screens_1_1WelcomeScreen.html#ae7e8a456383574cb46746b85036f2be9',1,'scxt::ui::app::other_screens::WelcomeScreen']]],
   ['what_2',['what',['../structscxt_1_1browser_1_1SQL_1_1Exception.html#abf843cbb29dec939d0731e491bab6f70',1,'scxt::browser::SQL::Exception::what()'],['../structscxt_1_1browser_1_1SQL_1_1LockedException.html#abf843cbb29dec939d0731e491bab6f70',1,'scxt::browser::SQL::LockedException::what()']]],
-  ['withlinearscaleformatting_3',['withlinearscaleformatting',['../group_8h.html#a0ea2b0b167942237e2c384201ce71c52',1,'withLinearScaleFormatting(&quot;keys&quot;)):&#160;group.h'],['../zone_8h.html#af85e8a4186cf275d834c318739883f48',1,'withLinearScaleFormatting(&quot;&quot;) .withDefault(-1) .withName(&quot;Pitch Bend Down&quot;) .withCustomMinDisplay(&quot;GRP&quot;)):&#160;zone.h']]],
+  ['withlinearscaleformatting_3',['withLinearScaleFormatting',['../zone_8h.html#af85e8a4186cf275d834c318739883f48',1,'zone.h']]],
   ['withname_4',['withName',['../zone_8h.html#a1c88d68c3940704476eeb1c6c61c40b2',1,'zone.h']]],
   ['workitemlineitem_5',['WorkItemLineItem',['../structscxt_1_1ui_1_1app_1_1missing__resolution_1_1WorkItemLineItem.html#a7883d04b672f8bf3d2136a420561e1a1',1,'scxt::ui::app::missing_resolution::WorkItemLineItem']]],
   ['writedebugmessage_6',['writeDebugMessage',['../structscxt_1_1browser_1_1BrowserDB.html#aec866111e323071d6e69c5e43db68e75',1,'scxt::browser::BrowserDB']]],
