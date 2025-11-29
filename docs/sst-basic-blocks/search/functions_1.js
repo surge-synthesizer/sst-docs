979f477c58d7bbb1dd31e3b93e1c246df5c61ab0
baconpaul <paul@baconpaul.org>
2025-11-29 11:12:05 +0000

    Update doxygen for sst-basic-blocks from github action

--- docs/sst-basic-blocks/search/functions_1.js
+++ docs/sst-basic-blocks/search/functions_1.js
@@ -7,6 +7,7 @@ var searchData=
   ['bindtargetbasevalue_4',['bindTargetBaseValue',['../structsst_1_1basic__blocks_1_1mod__matrix_1_1ModMatrix.html#a7172b4d7d30837310de6bf6c42bf6655',1,'sst::basic_blocks::mod_matrix::ModMatrix']]],
   ['blackman_5',['blackman',['../namespacesst_1_1basic__blocks_1_1dsp.html#a87d16b99c06c737b21a565703d5318fb',1,'sst::basic_blocks::dsp::blackman(int i, int n)'],['../namespacesst_1_1basic__blocks_1_1dsp.html#a2bc636992b5f5e81a50695f86efb8a7d',1,'sst::basic_blocks::dsp::blackman(double i, int n)']]],
   ['blackman_5fharris_6',['blackman_harris',['../namespacesst_1_1basic__blocks_1_1dsp.html#a776e2c2320e2ab56bbf3c250cf981702',1,'sst::basic_blocks::dsp::blackman_harris(int i, int n)'],['../namespacesst_1_1basic__blocks_1_1dsp.html#a5915265fcfe4c658ec12743b5426193b',1,'sst::basic_blocks::dsp::blackman_harris(double i, int n)']]],
-  ['blockabsmax_7',['blockAbsMax',['../namespacesst_1_1basic__blocks_1_1mechanics.html#a1f97a3b540f1bac3ed277920aacc7c2d',1,'sst::basic_blocks::mechanics']]],
-  ['boolean_8',['boolean',['../structsst_1_1basic__blocks_1_1dsp_1_1RNG.html#a54a211ac234172119d7ee8f89cb53750',1,'sst::basic_blocks::dsp::RNG']]]
+  ['blockabsavg_7',['blockAbsAvg',['../namespacesst_1_1basic__blocks_1_1mechanics.html#a5db7bddd83c1983475649edfa4792ffd',1,'sst::basic_blocks::mechanics']]],
+  ['blockabsmax_8',['blockAbsMax',['../namespacesst_1_1basic__blocks_1_1mechanics.html#a1f97a3b540f1bac3ed277920aacc7c2d',1,'sst::basic_blocks::mechanics']]],
+  ['boolean_9',['boolean',['../structsst_1_1basic__blocks_1_1dsp_1_1RNG.html#a54a211ac234172119d7ee8f89cb53750',1,'sst::basic_blocks::dsp::RNG']]]
 ];
