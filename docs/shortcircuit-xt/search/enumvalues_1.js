2db03ea53ae16d5ebde8f0244c09bb977673a9fb
baconpaul <paul@baconpaul.org>
2025-11-30 02:21:04 +0000

    Update doxygen for shortcircuit-xt from github action

--- docs/shortcircuit-xt/search/enumvalues_1.js
+++ docs/shortcircuit-xt/search/enumvalues_1.js
@@ -7,5 +7,6 @@ var searchData=
   ['bg_5f3_4',['bg_3',['../structscxt_1_1ui_1_1theme_1_1ColorMap.html#a0b96022cb9768c4cfbb8c14eb2a214e6a434823a2f8c4d3f554ade5f2635e49bf',1,'scxt::ui::theme::ColorMap']]],
   ['bonsai_5',['bonsai',['../namespacescxt_1_1engine.html#acefc6050a5b9c5c80a81b073b07bee54a665955ec94c32107a77bfd2471612bd3',1,'scxt::engine']]],
   ['brightness_6',['BRIGHTNESS',['../structscxt_1_1voice_1_1Voice.html#a9f6182ae4bcf0e9884551120d9efcef9a3a8a0aeb55d2886191dee8a5e0d07cc9',1,'scxt::voice::Voice']]],
-  ['browserautopreviewenabled_7',['browserAutoPreviewEnabled',['../namespacescxt_1_1infrastructure.html#a4028cef548b8de808e09d66ff5d004f4afc43963602ac63f719e0109c2f533576',1,'scxt::infrastructure']]]
+  ['browserautopreviewenabled_7',['browserAutoPreviewEnabled',['../namespacescxt_1_1infrastructure.html#a4028cef548b8de808e09d66ff5d004f4afc43963602ac63f719e0109c2f533576',1,'scxt::infrastructure']]],
+  ['browserpreviewamplitude_8',['browserPreviewAmplitude',['../namespacescxt_1_1infrastructure.html#a4028cef548b8de808e09d66ff5d004f4a01c973f9a6e10581872a3d6f1d436f5a',1,'scxt::infrastructure']]]
 ];
