04f4f532ba2f0b838b92795f3ab18afcfc64c8b1
baconpaul <paul@baconpaul.org>
2025-12-13 16:09:30 +0000

    Update doxygen for shortcircuit-xt from github action

--- docs/shortcircuit-xt/search/defines_0.js
+++ docs/shortcircuit-xt/search/defines_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['applyeg_0',['APPLYEG',['../sfz__import_8cpp.html#a56d4ccbc71e771cd9ca25a20d2d207d4',1,'sfz_import.cpp']]]
+  ['add_5ferror_5fmessage_0',['ADD_ERROR_MESSAGE',['../load__riff__wave_8cpp.html#a7303f3c63e4cb09389092f3ef88a2775',1,'load_riff_wave.cpp']]],
+  ['applyeg_1',['APPLYEG',['../sfz__import_8cpp.html#a56d4ccbc71e771cd9ca25a20d2d207d4',1,'sfz_import.cpp']]]
 ];
