1a1f6e1a42377429dcc865d8cc1aaa92ce03a2d9
baconpaul <paul@baconpaul.org>
2025-12-27 03:13:40 +0000

    Update doxygen for shortcircuit-xt from github action

--- docs/shortcircuit-xt/search/concepts_0.js
+++ docs/shortcircuit-xt/search/concepts_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['scxt_3a_3aengine_3a_3adtl_3a_3ahassilentsampleslength_0',['HasSilentSamplesLength',['../conceptscxt_1_1engine_1_1dtl_1_1HasSilentSamplesLength.html',1,'scxt::engine::dtl']]]
+  ['scxt_3a_3aengine_3a_3adtl_3a_3ahassilentsampleslength_0',['HasSilentSamplesLength',['../conceptscxt_1_1engine_1_1dtl_1_1HasSilentSamplesLength.html',1,'scxt::engine::dtl']]],
+  ['scxt_3a_3aui_3a_3aapp_3a_3ahasvaluealternate_1',['HasValueAlternate',['../conceptscxt_1_1ui_1_1app_1_1HasValueAlternate.html',1,'scxt::ui::app']]]
 ];
