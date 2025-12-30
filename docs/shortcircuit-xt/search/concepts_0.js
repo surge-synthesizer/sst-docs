5cc8f111fe8bef3cea7b851d7c04836502418df9
baconpaul <paul@baconpaul.org>
2025-12-30 18:49:34 +0000

    Update doxygen for shortcircuit-xt from github action

--- docs/shortcircuit-xt/search/concepts_0.js
+++ docs/shortcircuit-xt/search/concepts_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['scxt_3a_3aengine_3a_3adtl_3a_3ahassilentsampleslength_0',['HasSilentSamplesLength',['../conceptscxt_1_1engine_1_1dtl_1_1HasSilentSamplesLength.html',1,'scxt::engine::dtl']]],
-  ['scxt_3a_3aui_3a_3aapp_3a_3ahasvaluealternate_1',['HasValueAlternate',['../conceptscxt_1_1ui_1_1app_1_1HasValueAlternate.html',1,'scxt::ui::app']]]
+  ['scxt_3a_3aengine_3a_3adtl_3a_3abuseffectimplprovider_0',['BusEffectImplProvider',['../conceptscxt_1_1engine_1_1dtl_1_1BusEffectImplProvider.html',1,'scxt::engine::dtl']]],
+  ['scxt_3a_3aengine_3a_3adtl_3a_3ahassilentsampleslength_1',['HasSilentSamplesLength',['../conceptscxt_1_1engine_1_1dtl_1_1HasSilentSamplesLength.html',1,'scxt::engine::dtl']]],
+  ['scxt_3a_3aui_3a_3aapp_3a_3ahasvaluealternate_2',['HasValueAlternate',['../conceptscxt_1_1ui_1_1app_1_1HasValueAlternate.html',1,'scxt::ui::app']]]
 ];
--- docs/shortcircuit-xt/search/defines_4.js
+++ docs/shortcircuit-xt/search/defines_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['has_5fmemfn_0',['has_memfn',['../processor__impl_8h.html#a6a3517f8689319b560f466e60f89287e',1,'HAS_MEMFN:&#160;processor_impl.h'],['../bus__effect_8cpp.html#a6a3517f8689319b560f466e60f89287e',1,'HAS_MEMFN:&#160;bus_effect.cpp']]],
+  ['has_5fmemfn_0',['HAS_MEMFN',['../processor__impl_8h.html#a6a3517f8689319b560f466e60f89287e',1,'processor_impl.h']]],
   ['hdr_5fhelper_1',['HDR_HELPER',['../sfz__parse_8cpp.html#accb3e6fcc64ccb83ed375a8ec44ab6be',1,'sfz_parse.cpp']]]
 ];
