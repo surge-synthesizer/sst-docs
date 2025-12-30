caaec49d466190b30c2d249bea035667d79f29a2
baconpaul <paul@baconpaul.org>
2025-12-30 17:43:46 +0000

    Update doxygen for shortcircuit-xt from github action

--- docs/shortcircuit-xt/search/typedefs_2.js
+++ docs/shortcircuit-xt/search/typedefs_2.js
@@ -8,10 +8,9 @@ var searchData=
   ['colors_5ft_5',['colors_t',['../namespacescxt_1_1ui_1_1theme.html#a061789a2e4f07dedb208461fbbc27666',1,'scxt::ui::theme']]],
   ['combimpl_6',['combImpl',['../namespacescxt_1_1dsp_1_1processor_1_1procimpl_1_1detail.html#ae6a3842da05724ea8b2ed9d0e6de5251',1,'scxt::dsp::processor::procimpl::detail']]],
   ['combimpl_5fos_7',['combImpl_os',['../namespacescxt_1_1dsp_1_1processor_1_1procimpl_1_1detail.html#acee9beca8ab231712e54d6ab1dcb1a4e',1,'scxt::dsp::processor::procimpl::detail']]],
-  ['constcharop_5ft_8',['constCharOp_t',['../namespacescxt_1_1dsp_1_1processor_1_1detail.html#a140c0f02b7dcf505f39e084b30b3c951',1,'scxt::dsp::processor::detail']]],
-  ['coord_5ft_9',['coord_t',['../namespacescxt_1_1ui_1_1theme_1_1layout.html#a3d76b4343210f8e979227d575741b971',1,'scxt::ui::theme::layout']]],
-  ['copyprocessorleadpayload_5ft_10',['copyProcessorLeadPayload_t',['../namespacescxt_1_1messaging_1_1client.html#a5b2138848d7f76b47d0127cf25af6dcc',1,'scxt::messaging::client']]],
-  ['curveidentifier_11',['curveidentifier',['../structscxt_1_1modulation_1_1GroupMatrixConfig.html#a3c5915506a5a3534b7c3eafacbbfb92d',1,'scxt::modulation::GroupMatrixConfig::CurveIdentifier'],['../structscxt_1_1modulation_1_1ModulationCurves.html#ab8a83834a73c7c0a0861d00647a1113d',1,'scxt::modulation::ModulationCurves::CurveIdentifier'],['../structscxt_1_1voice_1_1modulation_1_1MatrixConfig.html#a3c5915506a5a3534b7c3eafacbbfb92d',1,'scxt::voice::modulation::MatrixConfig::CurveIdentifier']]],
-  ['cwarpimpl_12',['cWarpImpl',['../namespacescxt_1_1dsp_1_1processor_1_1procimpl_1_1detail.html#aafedfd998fbfa9eee620a3d56c7126df',1,'scxt::dsp::processor::procimpl::detail']]],
-  ['cwarpimpl_5fos_13',['cWarpImpl_os',['../namespacescxt_1_1dsp_1_1processor_1_1procimpl_1_1detail.html#aba6171da43dd8f774d23ba603e63a2e3',1,'scxt::dsp::processor::procimpl::detail']]]
+  ['coord_5ft_8',['coord_t',['../namespacescxt_1_1ui_1_1theme_1_1layout.html#a3d76b4343210f8e979227d575741b971',1,'scxt::ui::theme::layout']]],
+  ['copyprocessorleadpayload_5ft_9',['copyProcessorLeadPayload_t',['../namespacescxt_1_1messaging_1_1client.html#a5b2138848d7f76b47d0127cf25af6dcc',1,'scxt::messaging::client']]],
+  ['curveidentifier_10',['curveidentifier',['../structscxt_1_1modulation_1_1GroupMatrixConfig.html#a3c5915506a5a3534b7c3eafacbbfb92d',1,'scxt::modulation::GroupMatrixConfig::CurveIdentifier'],['../structscxt_1_1modulation_1_1ModulationCurves.html#ab8a83834a73c7c0a0861d00647a1113d',1,'scxt::modulation::ModulationCurves::CurveIdentifier'],['../structscxt_1_1voice_1_1modulation_1_1MatrixConfig.html#a3c5915506a5a3534b7c3eafacbbfb92d',1,'scxt::voice::modulation::MatrixConfig::CurveIdentifier']]],
+  ['cwarpimpl_11',['cWarpImpl',['../namespacescxt_1_1dsp_1_1processor_1_1procimpl_1_1detail.html#aafedfd998fbfa9eee620a3d56c7126df',1,'scxt::dsp::processor::procimpl::detail']]],
+  ['cwarpimpl_5fos_12',['cWarpImpl_os',['../namespacescxt_1_1dsp_1_1processor_1_1procimpl_1_1detail.html#aba6171da43dd8f774d23ba603e63a2e3',1,'scxt::dsp::processor::procimpl::detail']]]
 ];
