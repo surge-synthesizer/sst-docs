cb94d74816c7cb7ac4ea059d159788fa2fe7c706
baconpaul <paul@baconpaul.org>
2026-01-14 01:49:02 +0000

    Update doxygen for sst-basic-blocks from github action

--- docs/sst-basic-blocks/search/enums_8.js
+++ docs/sst-basic-blocks/search/enums_8.js
@@ -1,7 +1,6 @@
 var searchData=
 [
   ['shape_0',['Shape',['../structsst_1_1basic__blocks_1_1modulators_1_1SimpleLFO.html#a55b506070847a13554f8b879c1bfb37c',1,'sst::basic_blocks::modulators::SimpleLFO']]],
-  ['shuffletimes_1',['ShuffleTimes',['../namespacesst_1_1basic__blocks_1_1mechanics.html#a750049fe32363b0be8bb2e882466af12',1,'sst::basic_blocks::mechanics']]],
-  ['stage_2',['Stage',['../structsst_1_1basic__blocks_1_1modulators_1_1DiscreteStagesEnvelope.html#aca8133200c1122e29b87b314d82604eb',1,'sst::basic_blocks::modulators::DiscreteStagesEnvelope']]],
-  ['status_3',['Status',['../structsst_1_1basic__blocks_1_1modulators_1_1Transport.html#a67a0db04d321a74b7e7fcfd3f1a3f70b',1,'sst::basic_blocks::modulators::Transport']]]
+  ['stage_1',['Stage',['../structsst_1_1basic__blocks_1_1modulators_1_1DiscreteStagesEnvelope.html#aca8133200c1122e29b87b314d82604eb',1,'sst::basic_blocks::modulators::DiscreteStagesEnvelope']]],
+  ['status_2',['Status',['../structsst_1_1basic__blocks_1_1modulators_1_1Transport.html#a67a0db04d321a74b7e7fcfd3f1a3f70b',1,'sst::basic_blocks::modulators::Transport']]]
 ];
