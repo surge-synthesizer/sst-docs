ae8428505fd2290d54a89b8657538a43a1641306
baconpaul <paul@baconpaul.org>
2026-01-14 23:47:05 +0000

    Update doxygen for shortcircuit-xt from github action

--- docs/shortcircuit-xt/search/typedefs_d.js
+++ docs/shortcircuit-xt/search/typedefs_d.js
@@ -2,7 +2,9 @@ var searchData=
 [
   ['obx4impl_0',['obx4Impl',['../namespacescxt_1_1dsp_1_1processor_1_1procimpl_1_1detail.html#a10c52c8f3ccb7ee93445ebe0e190c648',1,'scxt::dsp::processor::procimpl::detail']]],
   ['obx4impl_5fos_1',['obx4Impl_os',['../namespacescxt_1_1dsp_1_1processor_1_1procimpl_1_1detail.html#a791539c0b04229d4fc53bcbba419a072',1,'scxt::dsp::processor::procimpl::detail']]],
-  ['ongui_5ft_2',['onGui_t',['../structscxt_1_1ui_1_1connectors_1_1DiscretePayloadDataAttachment.html#ada36d479302d937af647374dbb431fcd',1,'scxt::ui::connectors::DiscretePayloadDataAttachment']]],
-  ['opcodes_5ft_3',['opCodes_t',['../structscxt_1_1sfz__support_1_1SFZParser.html#a0565934d0720e1c5fdbd11dda447c34f',1,'scxt::sfz_support::SFZParser']]],
-  ['othertabselection_5ft_4',['otherTabSelection_t',['../structscxt_1_1selection_1_1SelectionManager.html#aed61bdfeb99338477d79a8685899b8dd',1,'scxt::selection::SelectionManager']]]
+  ['obxpandimpl_2',['obxpandImpl',['../namespacescxt_1_1dsp_1_1processor_1_1procimpl_1_1detail.html#a6f2e1be9d7958e9362023cc53576d42a',1,'scxt::dsp::processor::procimpl::detail']]],
+  ['obxpandimpl_5fos_3',['obxpandImpl_os',['../namespacescxt_1_1dsp_1_1processor_1_1procimpl_1_1detail.html#a881a65ba9cbc2665d49bc22d60675a37',1,'scxt::dsp::processor::procimpl::detail']]],
+  ['ongui_5ft_4',['onGui_t',['../structscxt_1_1ui_1_1connectors_1_1DiscretePayloadDataAttachment.html#ada36d479302d937af647374dbb431fcd',1,'scxt::ui::connectors::DiscretePayloadDataAttachment']]],
+  ['opcodes_5ft_5',['opCodes_t',['../structscxt_1_1sfz__support_1_1SFZParser.html#a0565934d0720e1c5fdbd11dda447c34f',1,'scxt::sfz_support::SFZParser']]],
+  ['othertabselection_5ft_6',['otherTabSelection_t',['../structscxt_1_1selection_1_1SelectionManager.html#aed61bdfeb99338477d79a8685899b8dd',1,'scxt::selection::SelectionManager']]]
 ];
