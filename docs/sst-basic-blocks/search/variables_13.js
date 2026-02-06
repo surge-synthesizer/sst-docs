085ca7944c250a1a137b1546857ef44c1c0b2218
baconpaul <paul@baconpaul.org>
2026-02-06 14:09:12 +0000

    Update doxygen for sst-basic-blocks from github action

--- docs/sst-basic-blocks/search/variables_13.js
+++ docs/sst-basic-blocks/search/variables_13.js
@@ -3,5 +3,6 @@ var searchData=
   ['u_0',['u',['../structsst_1_1basic__blocks_1_1dsp_1_1QuadratureOscillator.html#a253cb247773b8a286dd775cddced2161',1,'sst::basic_blocks::dsp::QuadratureOscillator']]],
   ['unit_1',['unit',['../structsst_1_1basic__blocks_1_1params_1_1ParamMetaData.html#a1729796f03559082040198b11c08f0ff',1,'sst::basic_blocks::params::ParamMetaData']]],
   ['unitseparator_2',['unitSeparator',['../structsst_1_1basic__blocks_1_1params_1_1ParamMetaData.html#a8efeb745ed08e8d00ba8859e08bac461',1,'sst::basic_blocks::params::ParamMetaData']]],
-  ['urng_3',['urng',['../structsst_1_1basic__blocks_1_1modulators_1_1SimpleLFO.html#aabb34c2ee05a0aaab7b9465931798971',1,'sst::basic_blocks::modulators::SimpleLFO']]]
+  ['unmodulatedvalues_3',['unmodulatedValues',['../structsst_1_1basic__blocks_1_1mod__matrix_1_1ModMatrix.html#a529b80447d8bb2fbe7877dba342cb3ff',1,'sst::basic_blocks::mod_matrix::ModMatrix']]],
+  ['urng_4',['urng',['../structsst_1_1basic__blocks_1_1modulators_1_1SimpleLFO.html#aabb34c2ee05a0aaab7b9465931798971',1,'sst::basic_blocks::modulators::SimpleLFO']]]
 ];
