b141c376dc0dda1334c0b955730a751182743c7c
baconpaul <paul@baconpaul.org>
2026-03-17 22:31:16 +0000

    Update doxygen for sst-basic-blocks from github action

--- docs/sst-basic-blocks/search/all_18.js
+++ docs/sst-basic-blocks/search/all_18.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['z_0',['z',['../structsst_1_1basic__blocks_1_1dsp_1_1Ballistics.html#af73583b1e980b0aa03f9884812e9fd4d',1,'sst::basic_blocks::dsp::Ballistics']]],
-  ['zero_1',['ZERO',['../structsst_1_1basic__blocks_1_1dsp_1_1quadDelayLine.html#a7be735855b2cd5e97d0bd0c28aabb0d8',1,'sst::basic_blocks::dsp::quadDelayLine']]]
+  ['zero_0',['ZERO',['../structsst_1_1basic__blocks_1_1dsp_1_1quadDelayLine.html#a7be735855b2cd5e97d0bd0c28aabb0d8',1,'sst::basic_blocks::dsp::quadDelayLine']]],
+  ['zl_1',['zL',['../structsst_1_1basic__blocks_1_1dsp_1_1Ballistics.html#a7c1d7868040ed66b2dfeae7f64bdf7af',1,'sst::basic_blocks::dsp::Ballistics']]],
+  ['zr_2',['zR',['../structsst_1_1basic__blocks_1_1dsp_1_1Ballistics.html#aa915510537a91b561f62ae75a0cfed25',1,'sst::basic_blocks::dsp::Ballistics']]]
 ];
