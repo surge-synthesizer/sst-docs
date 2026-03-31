c18b1f746c60c9021c5bcb3eb3c0fb5e6027c26b
baconpaul <paul@baconpaul.org>
2026-03-31 23:07:02 +0000

    Update doxygen for sst-voicemanager from github action

--- docs/sst-voicemanager/search/all_a.js
+++ docs/sst-voicemanager/search/all_a.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['lastpbbychannel_0',['lastPBByChannel',['../structsst_1_1voicemanager_1_1VoiceManager_1_1Details.html#a0da06d7ac9c97590ff3878511ed774f0',1,'sst::voicemanager::VoiceManager::Details']]],
-  ['lowest_1',['LOWEST',['../structsst_1_1voicemanager_1_1VoiceManager.html#a8fe62f239d82bfcbfd6f04d3a1f41e60a664626f39fdbfdb9cfe98285a9e161f9',1,'sst::voicemanager::VoiceManager']]]
+  ['latest_1',['LATEST',['../structsst_1_1voicemanager_1_1VoiceManager.html#a54760f344ea12f29a7bdbb1ec80553f4a402c00b13906631809870b721330e84c',1,'sst::voicemanager::VoiceManager']]],
+  ['lowest_2',['lowest',['../structsst_1_1voicemanager_1_1VoiceManager.html#a8fe62f239d82bfcbfd6f04d3a1f41e60a664626f39fdbfdb9cfe98285a9e161f9',1,'sst::voicemanager::VoiceManager::LOWEST'],['../structsst_1_1voicemanager_1_1VoiceManager.html#a54760f344ea12f29a7bdbb1ec80553f4a664626f39fdbfdb9cfe98285a9e161f9',1,'sst::voicemanager::VoiceManager::LOWEST']]]
 ];
--- docs/sst-voicemanager/search/enums_1.js
+++ docs/sst-voicemanager/search/enums_1.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['midi1dialect_0',['MIDI1Dialect',['../structsst_1_1voicemanager_1_1VoiceManager.html#adef5d969ee0c8fd79b4d073310cdfb6c',1,'sst::voicemanager::VoiceManager']]],
-  ['monoplaymodefeatures_1',['MonoPlayModeFeatures',['../structsst_1_1voicemanager_1_1VoiceManager.html#af6cc76daccd7eafe7d70e76061f99ed0',1,'sst::voicemanager::VoiceManager']]]
+  ['monoplaymodefeatures_1',['MonoPlayModeFeatures',['../structsst_1_1voicemanager_1_1VoiceManager.html#af6cc76daccd7eafe7d70e76061f99ed0',1,'sst::voicemanager::VoiceManager']]],
+  ['monoprioritymode_2',['MonoPriorityMode',['../structsst_1_1voicemanager_1_1VoiceManager.html#a54760f344ea12f29a7bdbb1ec80553f4',1,'sst::voicemanager::VoiceManager']]]
 ];
