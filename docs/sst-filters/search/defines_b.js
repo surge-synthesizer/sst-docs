e37b3391746741ed7354cba773b2120333ac6f19
baconpaul <paul@baconpaul.org>
2026-05-07 17:21:07 +0000

    Update doxygen for sst-filters from github action

--- docs/sst-filters/search/defines_b.js
+++ docs/sst-filters/search/defines_b.js
@@ -5,7 +5,7 @@ var searchData=
   ['scenario_5fmethod_2',['SCENARIO_METHOD',['../catch2_8hpp.html#add17eb8f8d85412a08a8a048cd38f33b',1,'catch2.hpp']]],
   ['section_3',['SECTION',['../catch2_8hpp.html#ad512fd95a78b95770b9759823f8fbc21',1,'catch2.hpp']]],
   ['setall_4',['setall',['../CytomicTilt_8h.html#a24945dfd73b918c244cfaa0ce54f5cbb',1,'SETALL:&#160;CytomicTilt.h'],['../FastTiltNoiseFilter_8h.html#a24945dfd73b918c244cfaa0ce54f5cbb',1,'SETALL:&#160;FastTiltNoiseFilter.h'],['../filters_2CytomicSVF_8h.html#a24945dfd73b918c244cfaa0ce54f5cbb',1,'SETALL:&#160;CytomicSVF.h']]],
-  ['simde_5fenable_5fnative_5faliases_5',['simde_enable_native_aliases',['../filters_2CytomicSVF_8h.html#a3d7b10d1c02ef12d64d7a6fa90e9a1e9',1,'SIMDE_ENABLE_NATIVE_ALIASES:&#160;CytomicSVF.h'],['../CytomicTilt_8h.html#a3d7b10d1c02ef12d64d7a6fa90e9a1e9',1,'SIMDE_ENABLE_NATIVE_ALIASES:&#160;CytomicTilt.h'],['../FastTiltNoiseFilter_8h.html#a3d7b10d1c02ef12d64d7a6fa90e9a1e9',1,'SIMDE_ENABLE_NATIVE_ALIASES:&#160;FastTiltNoiseFilter.h']]],
+  ['simde_5fenable_5fnative_5faliases_5',['simde_enable_native_aliases',['../filters_2CytomicSVF_8h.html#a3d7b10d1c02ef12d64d7a6fa90e9a1e9',1,'SIMDE_ENABLE_NATIVE_ALIASES:&#160;CytomicSVF.h'],['../CytomicTilt_8h.html#a3d7b10d1c02ef12d64d7a6fa90e9a1e9',1,'SIMDE_ENABLE_NATIVE_ALIASES:&#160;CytomicTilt.h']]],
   ['sst_5ffilters_5fcomb_5fextension_5ffactor_6',['SST_FILTERS_COMB_EXTENSION_FACTOR',['../globals_8h.html#af814629a0dda8079fb54801566efe580',1,'globals.h']]],
   ['static_5frequire_7',['STATIC_REQUIRE',['../catch2_8hpp.html#abad9ff23b730469f209b010e0ac4687c',1,'catch2.hpp']]],
   ['static_5frequire_5ffalse_8',['STATIC_REQUIRE_FALSE',['../catch2_8hpp.html#ae7506af68f12e7efdb22e951b911b5a0',1,'catch2.hpp']]],
