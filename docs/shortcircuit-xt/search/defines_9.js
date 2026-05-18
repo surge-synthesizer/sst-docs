a7c59c12f5639b0a744cce470945007adc009cce
baconpaul <paul@baconpaul.org>
2026-05-18 14:40:04 +0000

    Update doxygen for shortcircuit-xt from github action

--- docs/shortcircuit-xt/search/defines_9.js
+++ docs/shortcircuit-xt/search/defines_9.js
@@ -1,5 +1,7 @@
 var searchData=
 [
   ['raise_5ferror_5fcont_0',['RAISE_ERROR_CONT',['../utils_8h.html#ace79af729b9eeb0f5538604349a848ef',1,'utils.h']]],
-  ['raise_5ferror_5fengine_1',['RAISE_ERROR_ENGINE',['../utils_8h.html#adfc9736c98bfc337566f0ad14b7bda71',1,'utils.h']]]
+  ['raise_5ferror_5fengine_1',['RAISE_ERROR_ENGINE',['../utils_8h.html#adfc9736c98bfc337566f0ad14b7bda71',1,'utils.h']]],
+  ['raise_5finfo_5fcont_2',['RAISE_INFO_CONT',['../utils_8h.html#a1d8e172d0daab3bd5166f34743fd8b57',1,'utils.h']]],
+  ['raise_5fwarn_5fcont_3',['RAISE_WARN_CONT',['../utils_8h.html#adcdbabb7169c11cde9352d3be99dd13b',1,'utils.h']]]
 ];
--- docs/shortcircuit-xt/search/enums_e.js
+++ docs/shortcircuit-xt/search/enums_e.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['ruleidx_0',['RuleIdx',['../structscxt_1_1ui_1_1app_1_1edit__screen_1_1MappingDisplay.html#aa9681d8287b7110e5223863dce73a2dc',1,'scxt::ui::app::edit_screen::MappingDisplay']]]
+  ['reportitemseverity_0',['ReportItemSeverity',['../namespacescxt_1_1messaging_1_1client.html#aa431d89caf65910b25049ece6b702420',1,'scxt::messaging::client']]],
+  ['ruleidx_1',['RuleIdx',['../structscxt_1_1ui_1_1app_1_1edit__screen_1_1MappingDisplay.html#aa9681d8287b7110e5223863dce73a2dc',1,'scxt::ui::app::edit_screen::MappingDisplay']]]
 ];
