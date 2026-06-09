beb3af5915598639b68a8376bbe9712f8e6cd594
baconpaul <paul@baconpaul.org>
2026-06-09 16:36:11 +0000

    Update doxygen for sst-voicemanager from github action

--- docs/sst-voicemanager/search/all_6.js
+++ docs/sst-voicemanager/search/all_6.js
@@ -3,6 +3,7 @@ var searchData=
   ['gated_0',['gated',['../structsst_1_1voicemanager_1_1VoiceManager_1_1Details_1_1VoiceInfo.html#a916cd9419d627e709cdfd07f2d8e1e8f',1,'sst::voicemanager::VoiceManager::Details::VoiceInfo']]],
   ['gatedduetosustain_1',['gatedDueToSustain',['../structsst_1_1voicemanager_1_1VoiceManager_1_1Details_1_1VoiceInfo.html#aedecfe326b14c1e0319d7740e483c740',1,'sst::voicemanager::VoiceManager::Details::VoiceInfo']]],
   ['getgatedvoicecount_2',['getGatedVoiceCount',['../structsst_1_1voicemanager_1_1VoiceManager.html#ac7a9edd01bb9cc9ed25cfac0f3e50ca5',1,'sst::voicemanager::VoiceManager']]],
-  ['getvoicecount_3',['getVoiceCount',['../structsst_1_1voicemanager_1_1VoiceManager.html#aadeab3cc6a1851786391c07c1c83d36f',1,'sst::voicemanager::VoiceManager']]],
-  ['guaranteegroup_4',['guaranteegroup',['../structsst_1_1voicemanager_1_1VoiceManager.html#a8ef62de268f4d9014a08633d9d475ef5',1,'sst::voicemanager::VoiceManager::guaranteeGroup()'],['../structsst_1_1voicemanager_1_1VoiceManager_1_1Details.html#a8ef62de268f4d9014a08633d9d475ef5',1,'sst::voicemanager::VoiceManager::Details::guaranteeGroup()']]]
+  ['getpolyphonygroupvoicelimit_3',['getPolyphonyGroupVoiceLimit',['../structsst_1_1voicemanager_1_1VoiceManager.html#a089fdbdebae320c0981455438282e3fd',1,'sst::voicemanager::VoiceManager']]],
+  ['getvoicecount_4',['getVoiceCount',['../structsst_1_1voicemanager_1_1VoiceManager.html#aadeab3cc6a1851786391c07c1c83d36f',1,'sst::voicemanager::VoiceManager']]],
+  ['guaranteegroup_5',['guaranteegroup',['../structsst_1_1voicemanager_1_1VoiceManager.html#a8ef62de268f4d9014a08633d9d475ef5',1,'sst::voicemanager::VoiceManager::guaranteeGroup()'],['../structsst_1_1voicemanager_1_1VoiceManager_1_1Details.html#a8ef62de268f4d9014a08633d9d475ef5',1,'sst::voicemanager::VoiceManager::Details::guaranteeGroup()']]]
 ];
--- docs/sst-voicemanager/search/functions_4.js
+++ docs/sst-voicemanager/search/functions_4.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['getgatedvoicecount_0',['getGatedVoiceCount',['../structsst_1_1voicemanager_1_1VoiceManager.html#ac7a9edd01bb9cc9ed25cfac0f3e50ca5',1,'sst::voicemanager::VoiceManager']]],
-  ['getvoicecount_1',['getVoiceCount',['../structsst_1_1voicemanager_1_1VoiceManager.html#aadeab3cc6a1851786391c07c1c83d36f',1,'sst::voicemanager::VoiceManager']]],
-  ['guaranteegroup_2',['guaranteegroup',['../structsst_1_1voicemanager_1_1VoiceManager.html#a8ef62de268f4d9014a08633d9d475ef5',1,'sst::voicemanager::VoiceManager::guaranteeGroup()'],['../structsst_1_1voicemanager_1_1VoiceManager_1_1Details.html#a8ef62de268f4d9014a08633d9d475ef5',1,'sst::voicemanager::VoiceManager::Details::guaranteeGroup()']]]
+  ['getpolyphonygroupvoicelimit_1',['getPolyphonyGroupVoiceLimit',['../structsst_1_1voicemanager_1_1VoiceManager.html#a089fdbdebae320c0981455438282e3fd',1,'sst::voicemanager::VoiceManager']]],
+  ['getvoicecount_2',['getVoiceCount',['../structsst_1_1voicemanager_1_1VoiceManager.html#aadeab3cc6a1851786391c07c1c83d36f',1,'sst::voicemanager::VoiceManager']]],
+  ['guaranteegroup_3',['guaranteegroup',['../structsst_1_1voicemanager_1_1VoiceManager.html#a8ef62de268f4d9014a08633d9d475ef5',1,'sst::voicemanager::VoiceManager::guaranteeGroup()'],['../structsst_1_1voicemanager_1_1VoiceManager_1_1Details.html#a8ef62de268f4d9014a08633d9d475ef5',1,'sst::voicemanager::VoiceManager::Details::guaranteeGroup()']]]
 ];
