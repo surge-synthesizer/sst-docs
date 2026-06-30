80df1712da5686612b9437af121598b3a49346ec
baconpaul <paul@baconpaul.org>
2026-06-30 23:33:33 +0000

    Update doxygen for sst-basic-blocks from github action

--- docs/sst-basic-blocks/search/classes_a.js
+++ docs/sst-basic-blocks/search/classes_a.js
@@ -1,6 +1,8 @@
 var searchData=
 [
-  ['onepolelag_0',['OnePoleLag',['../structsst_1_1basic__blocks_1_1dsp_1_1OnePoleLag.html',1,'sst::basic_blocks::dsp']]],
-  ['onepolelag_3c_20float_2c_20false_20_3e_1',['OnePoleLag&lt; float, false &gt;',['../structsst_1_1basic__blocks_1_1dsp_1_1OnePoleLag.html',1,'sst::basic_blocks::dsp']]],
-  ['onepolelag_3c_20t_2c_20true_20_3e_2',['OnePoleLag&lt; T, true &gt;',['../structsst_1_1basic__blocks_1_1dsp_1_1OnePoleLag.html',1,'sst::basic_blocks::dsp']]]
+  ['onepolehp_0',['OnePoleHP',['../structsst_1_1basic__blocks_1_1dsp_1_1OnePoleHP.html',1,'sst::basic_blocks::dsp']]],
+  ['onepolelag_1',['OnePoleLag',['../structsst_1_1basic__blocks_1_1dsp_1_1OnePoleLag.html',1,'sst::basic_blocks::dsp']]],
+  ['onepolelag_3c_20float_2c_20false_20_3e_2',['OnePoleLag&lt; float, false &gt;',['../structsst_1_1basic__blocks_1_1dsp_1_1OnePoleLag.html',1,'sst::basic_blocks::dsp']]],
+  ['onepolelag_3c_20t_2c_20true_20_3e_3',['OnePoleLag&lt; T, true &gt;',['../structsst_1_1basic__blocks_1_1dsp_1_1OnePoleLag.html',1,'sst::basic_blocks::dsp']]],
+  ['onepolelp_4',['OnePoleLP',['../structsst_1_1basic__blocks_1_1dsp_1_1OnePoleLP.html',1,'sst::basic_blocks::dsp']]]
 ];
--- docs/sst-basic-blocks/search/files_a.js
+++ docs/sst-basic-blocks/search/files_a.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['oscillatordriftunisoncharacter_2eh_0',['OscillatorDriftUnisonCharacter.h',['../OscillatorDriftUnisonCharacter_8h.html',1,'']]]
+  ['onepoles_2eh_0',['OnePoles.h',['../OnePoles_8h.html',1,'']]],
+  ['oscillatordriftunisoncharacter_2eh_1',['OscillatorDriftUnisonCharacter.h',['../OscillatorDriftUnisonCharacter_8h.html',1,'']]]
 ];
