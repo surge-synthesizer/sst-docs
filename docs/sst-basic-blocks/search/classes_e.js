ec0f4e20ecd931d27fad5a3e102366455dad7bdf
baconpaul <paul@baconpaul.org>
2026-07-22 20:41:22 +0000

    Update doxygen for sst-basic-blocks from github action

--- docs/sst-basic-blocks/search/classes_e.js
+++ docs/sst-basic-blocks/search/classes_e.js
@@ -2,15 +2,16 @@ var searchData=
 [
   ['shortcircuitsinctableprovider_0',['ShortcircuitSincTableProvider',['../structsst_1_1basic__blocks_1_1tables_1_1ShortcircuitSincTableProvider.html',1,'sst::basic_blocks::tables']]],
   ['signature_1',['Signature',['../structsst_1_1basic__blocks_1_1modulators_1_1Transport_1_1Signature.html',1,'sst::basic_blocks::modulators::Transport']]],
-  ['simplelfo_2',['SimpleLFO',['../structsst_1_1basic__blocks_1_1modulators_1_1SimpleLFO.html',1,'sst::basic_blocks::modulators']]],
-  ['simplesineprovider_3',['SimpleSineProvider',['../structsst_1_1basic__blocks_1_1tables_1_1SimpleSineProvider.html',1,'sst::basic_blocks::tables']]],
-  ['sixsineswaveprovider_4',['SixSinesWaveProvider',['../structsst_1_1basic__blocks_1_1tables_1_1SixSinesWaveProvider.html',1,'sst::basic_blocks::tables']]],
-  ['slewlimiter_5',['SlewLimiter',['../structsst_1_1basic__blocks_1_1dsp_1_1SlewLimiter.html',1,'sst::basic_blocks::dsp']]],
-  ['ssesincdelayline_6',['SSESincDelayLine',['../structsst_1_1basic__blocks_1_1dsp_1_1SSESincDelayLine.html',1,'sst::basic_blocks::dsp']]],
-  ['ssesincinterpolater_7',['SSESincInterpolater',['../structsst_1_1basic__blocks_1_1dsp_1_1SSESincInterpolater.html',1,'sst::basic_blocks::dsp']]],
-  ['steplfo_8',['StepLFO',['../structsst_1_1basic__blocks_1_1modulators_1_1StepLFO.html',1,'sst::basic_blocks::modulators']]],
-  ['storage_9',['Storage',['../structsst_1_1basic__blocks_1_1modulators_1_1StepLFO_1_1Storage.html',1,'sst::basic_blocks::modulators::StepLFO']]],
-  ['surgelag_10',['SurgeLag',['../structsst_1_1basic__blocks_1_1dsp_1_1SurgeLag.html',1,'sst::basic_blocks::dsp']]],
-  ['surgequadrosc_11',['SurgeQuadrOsc',['../structsst_1_1basic__blocks_1_1dsp_1_1SurgeQuadrOsc.html',1,'sst::basic_blocks::dsp']]],
-  ['surgesinctableprovider_12',['SurgeSincTableProvider',['../structsst_1_1basic__blocks_1_1tables_1_1SurgeSincTableProvider.html',1,'sst::basic_blocks::tables']]]
+  ['simplelerp_2',['SimpleLerp',['../structsst_1_1basic__blocks_1_1dsp_1_1SimpleLerp.html',1,'sst::basic_blocks::dsp']]],
+  ['simplelfo_3',['SimpleLFO',['../structsst_1_1basic__blocks_1_1modulators_1_1SimpleLFO.html',1,'sst::basic_blocks::modulators']]],
+  ['simplesineprovider_4',['SimpleSineProvider',['../structsst_1_1basic__blocks_1_1tables_1_1SimpleSineProvider.html',1,'sst::basic_blocks::tables']]],
+  ['sixsineswaveprovider_5',['SixSinesWaveProvider',['../structsst_1_1basic__blocks_1_1tables_1_1SixSinesWaveProvider.html',1,'sst::basic_blocks::tables']]],
+  ['slewlimiter_6',['SlewLimiter',['../structsst_1_1basic__blocks_1_1dsp_1_1SlewLimiter.html',1,'sst::basic_blocks::dsp']]],
+  ['ssesincdelayline_7',['SSESincDelayLine',['../structsst_1_1basic__blocks_1_1dsp_1_1SSESincDelayLine.html',1,'sst::basic_blocks::dsp']]],
+  ['ssesincinterpolater_8',['SSESincInterpolater',['../structsst_1_1basic__blocks_1_1dsp_1_1SSESincInterpolater.html',1,'sst::basic_blocks::dsp']]],
+  ['steplfo_9',['StepLFO',['../structsst_1_1basic__blocks_1_1modulators_1_1StepLFO.html',1,'sst::basic_blocks::modulators']]],
+  ['storage_10',['Storage',['../structsst_1_1basic__blocks_1_1modulators_1_1StepLFO_1_1Storage.html',1,'sst::basic_blocks::modulators::StepLFO']]],
+  ['surgelag_11',['SurgeLag',['../structsst_1_1basic__blocks_1_1dsp_1_1SurgeLag.html',1,'sst::basic_blocks::dsp']]],
+  ['surgequadrosc_12',['SurgeQuadrOsc',['../structsst_1_1basic__blocks_1_1dsp_1_1SurgeQuadrOsc.html',1,'sst::basic_blocks::dsp']]],
+  ['surgesinctableprovider_13',['SurgeSincTableProvider',['../structsst_1_1basic__blocks_1_1tables_1_1SurgeSincTableProvider.html',1,'sst::basic_blocks::tables']]]
 ];
--- docs/sst-basic-blocks/search/functions_5.js
+++ docs/sst-basic-blocks/search/functions_5.js
@@ -19,13 +19,14 @@ var searchData=
   ['fasttanhsseclamped_16',['fasttanhSSEclamped',['../namespacesst_1_1basic__blocks_1_1dsp.html#a9dfb5fa4053b06de48f1aaeb5ce883eb',1,'sst::basic_blocks::dsp']]],
   ['fasttansse_17',['fasttanSSE',['../namespacesst_1_1basic__blocks_1_1dsp.html#a28991946705150b3973b2065271cb0b5',1,'sst::basic_blocks::dsp']]],
   ['featurestate_18',['FeatureState',['../structsst_1_1basic__blocks_1_1params_1_1ParamMetaData_1_1FeatureState.html#a0e32b948bff830fce8f771b2c9a89c25',1,'sst::basic_blocks::params::ParamMetaData::FeatureState']]],
-  ['fillblock_19',['fillBlock',['../structsst_1_1basic__blocks_1_1dsp_1_1DPWSawOscillator.html#abeebc3c44ea4e76dd9338206ff3e2af3',1,'sst::basic_blocks::dsp::DPWSawOscillator']]],
-  ['filltable_20',['fillTable',['../structsst_1_1basic__blocks_1_1tables_1_1SixSinesWaveProvider.html#a07579c2347ae823d57fa1369bb54299f',1,'sst::basic_blocks::tables::SixSinesWaveProvider']]],
-  ['filter_21',['filter',['../structsst_1_1basic__blocks_1_1dsp_1_1DCBlocker.html#a7cc05a2d7dc1581885c818581f376b0c',1,'sst::basic_blocks::dsp::DCBlocker']]],
-  ['fordisplay_22',['forDisplay',['../structsst_1_1basic__blocks_1_1dsp_1_1RNG.html#aa1c9be9e05e258ea63e85ebd79358727',1,'sst::basic_blocks::dsp::RNG']]],
-  ['freeze_23',['freeze',['../structsst_1_1basic__blocks_1_1modulators_1_1SimpleLFO.html#abd8698b462ce90fe56b15ce7a0192d3e',1,'sst::basic_blocks::modulators::SimpleLFO']]],
-  ['fromfloat_24',['fromfloat',['../structsst_1_1basic__blocks_1_1tables_1_1temposync_1_1ZeroOne.html#a1ce38028f3fbcc3a635ccc1db723adfe',1,'sst::basic_blocks::tables::temposync::ZeroOne::fromFloat()'],['../structsst_1_1basic__blocks_1_1tables_1_1temposync_1_1TwoToThe.html#a81ba7ba2ba1fd3ca77328d5bf0202960',1,'sst::basic_blocks::tables::temposync::TwoToThe::fromFloat()']]],
-  ['fromrawarray_25',['fromRawArray',['../structsst_1_1basic__blocks_1_1simd_1_1F32x4.html#a37f93981fa090835e5ba1e119df521cf',1,'sst::basic_blocks::simd::F32x4']]],
-  ['fromstring_26',['fromString',['../namespacesst_1_1basic__blocks_1_1tables_1_1temposync.html#a09d5a7cb27958eee0e4d0172c63fb00a',1,'sst::basic_blocks::tables::temposync']]],
-  ['fxmodcontrol_27',['fxmodcontrol',['../structsst_1_1basic__blocks_1_1modulators_1_1FXModControl.html#a42aedac965b8e768535e1d50e3f08bc5',1,'sst::basic_blocks::modulators::FXModControl::FXModControl()'],['../structsst_1_1basic__blocks_1_1modulators_1_1FXModControl.html#a76cab151698b6ec91bf7f07fd93c3227',1,'sst::basic_blocks::modulators::FXModControl::FXModControl(float sr, float sri, float *sineTable=nullptr)']]]
+  ['fill_19',['fill',['../structsst_1_1basic__blocks_1_1dsp_1_1SimpleLerp.html#a7e42c254bbfd85048dfa442bfcb18bbc',1,'sst::basic_blocks::dsp::SimpleLerp']]],
+  ['fillblock_20',['fillBlock',['../structsst_1_1basic__blocks_1_1dsp_1_1DPWSawOscillator.html#abeebc3c44ea4e76dd9338206ff3e2af3',1,'sst::basic_blocks::dsp::DPWSawOscillator']]],
+  ['filltable_21',['fillTable',['../structsst_1_1basic__blocks_1_1tables_1_1SixSinesWaveProvider.html#a07579c2347ae823d57fa1369bb54299f',1,'sst::basic_blocks::tables::SixSinesWaveProvider']]],
+  ['filter_22',['filter',['../structsst_1_1basic__blocks_1_1dsp_1_1DCBlocker.html#a7cc05a2d7dc1581885c818581f376b0c',1,'sst::basic_blocks::dsp::DCBlocker']]],
+  ['fordisplay_23',['forDisplay',['../structsst_1_1basic__blocks_1_1dsp_1_1RNG.html#aa1c9be9e05e258ea63e85ebd79358727',1,'sst::basic_blocks::dsp::RNG']]],
+  ['freeze_24',['freeze',['../structsst_1_1basic__blocks_1_1modulators_1_1SimpleLFO.html#abd8698b462ce90fe56b15ce7a0192d3e',1,'sst::basic_blocks::modulators::SimpleLFO']]],
+  ['fromfloat_25',['fromfloat',['../structsst_1_1basic__blocks_1_1tables_1_1temposync_1_1ZeroOne.html#a1ce38028f3fbcc3a635ccc1db723adfe',1,'sst::basic_blocks::tables::temposync::ZeroOne::fromFloat()'],['../structsst_1_1basic__blocks_1_1tables_1_1temposync_1_1TwoToThe.html#a81ba7ba2ba1fd3ca77328d5bf0202960',1,'sst::basic_blocks::tables::temposync::TwoToThe::fromFloat()']]],
+  ['fromrawarray_26',['fromRawArray',['../structsst_1_1basic__blocks_1_1simd_1_1F32x4.html#a37f93981fa090835e5ba1e119df521cf',1,'sst::basic_blocks::simd::F32x4']]],
+  ['fromstring_27',['fromString',['../namespacesst_1_1basic__blocks_1_1tables_1_1temposync.html#a09d5a7cb27958eee0e4d0172c63fb00a',1,'sst::basic_blocks::tables::temposync']]],
+  ['fxmodcontrol_28',['fxmodcontrol',['../structsst_1_1basic__blocks_1_1modulators_1_1FXModControl.html#a42aedac965b8e768535e1d50e3f08bc5',1,'sst::basic_blocks::modulators::FXModControl::FXModControl()'],['../structsst_1_1basic__blocks_1_1modulators_1_1FXModControl.html#a76cab151698b6ec91bf7f07fd93c3227',1,'sst::basic_blocks::modulators::FXModControl::FXModControl(float sr, float sri, float *sineTable=nullptr)']]]
 ];
