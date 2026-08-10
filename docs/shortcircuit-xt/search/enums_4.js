afe9bc8053e134fc8dd888ea27ba81da6b06d591
baconpaul <paul@baconpaul.org>
2026-08-10 17:00:54 +0000

    Update doxygen for shortcircuit-xt from github action

--- docs/shortcircuit-xt/search/enums_4.js
+++ docs/shortcircuit-xt/search/enums_4.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['editsubtree_0',['EditSubtree',['../namespacescxt_1_1messaging_1_1client.html#a2d1e2f23fe578da23e2809d174b8cb38',1,'scxt::messaging::client']]],
   ['egwhich_1',['EGWhich',['../namespacescxt_1_1import__support.html#a962527988d5911b8c39298c0795007c6',1,'scxt::import_support']]],
-  ['expressionids_2',['ExpressionIDs',['../structscxt_1_1voice_1_1Voice.html#a9f6182ae4bcf0e9884551120d9efcef9',1,'scxt::voice::Voice']]],
-  ['exsparam_3',['EXSParam',['../namespacescxt_1_1exs__support.html#aa720d0a28ea34b938684edc6b674c6a2',1,'scxt::exs_support']]]
+  ['emptyslot_2',['EmptySlot',['../structscxt_1_1ui_1_1app_1_1edit__screen_1_1VariantDisplay.html#a9fae6fa1ec95a54c068c5e2f6746925c',1,'scxt::ui::app::edit_screen::VariantDisplay']]],
+  ['expressionids_3',['ExpressionIDs',['../structscxt_1_1voice_1_1Voice.html#a9f6182ae4bcf0e9884551120d9efcef9',1,'scxt::voice::Voice']]],
+  ['exsparam_4',['EXSParam',['../namespacescxt_1_1exs__support.html#aa720d0a28ea34b938684edc6b674c6a2',1,'scxt::exs_support']]]
 ];
