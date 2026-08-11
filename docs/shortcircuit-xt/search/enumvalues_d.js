31d628ea4efacd0eeb6b78d221095ecf9eb3c0aa
baconpaul <paul@baconpaul.org>
2026-08-11 15:28:47 +0000

    Update doxygen for shortcircuit-xt from github action

--- docs/shortcircuit-xt/search/enumvalues_d.js
+++ docs/shortcircuit-xt/search/enumvalues_d.js
@@ -5,13 +5,12 @@ var searchData=
   ['omni_2',['OMNI',['../structscxt_1_1engine_1_1Engine.html#a1d429ac041d710e8a840a103b12d9c18a63389071b9b8e84b1c40330b2eeecac0',1,'scxt::engine::Engine']]],
   ['omniflavor_3',['omniFlavor',['../namespacescxt_1_1infrastructure.html#a4028cef548b8de808e09d66ff5d004f4ac3e0d3f9321bb9de8d8e5ae02f1f39c1',1,'scxt::infrastructure']]],
   ['on_5frelease_4',['ON_RELEASE',['../structscxt_1_1engine_1_1Zone.html#a188092ed559d356ade9a517d60ec3f34a612203253b887081ce8d008feaa45a08',1,'scxt::engine::Zone']]],
-  ['one_5fshot_5',['ONE_SHOT',['../structscxt_1_1engine_1_1Zone.html#a188092ed559d356ade9a517d60ec3f34a811e55ae9fced8f0b5d4be29bbe0775b',1,'scxt::engine::Zone']]],
-  ['oneshot_6',['oneshot',['../structscxt_1_1modulation_1_1ModulatorStorage.html#a22823e777472311df6b822c4393c6127a2724fa87f252403cd2c93f7437f34fd5',1,'scxt::modulation::ModulatorStorage::ONESHOT'],['../structscxt_1_1modulation_1_1modulators_1_1AdsrStorage.html#a72379015e04e7913b42264153319af1cae875b67b322212bf81da51e697bc4450',1,'scxt::modulation::modulators::AdsrStorage::ONESHOT']]],
-  ['only_5fcollect_7',['ONLY_COLLECT',['../namespacescxt_1_1patch__io.html#a3581f6f1928496586a0cac5def142324abfcb04013527a1baf393ee4843c4f56e',1,'scxt::patch_io']]],
-  ['opus_5ffile_8',['OPUS_FILE',['../structscxt_1_1sample_1_1Sample.html#a3ae727773c367ac1041d72ac770a0ab1a839ddadb16cc4ed728d40f1d009df393',1,'scxt::sample::Sample']]],
-  ['or_9',['OR',['../structscxt_1_1engine_1_1GroupTriggerConditions.html#a4bf3727f5c3c89a9e7d3b88969ce767aa1d00e7dce692e8dc3f6877f035e3a616',1,'scxt::engine::GroupTriggerConditions']]],
-  ['or_5fnot_10',['OR_NOT',['../structscxt_1_1engine_1_1GroupTriggerConditions.html#a4bf3727f5c3c89a9e7d3b88969ce767aa756eaf1e3554bffb0df4a824191577f6',1,'scxt::engine::GroupTriggerConditions']]],
-  ['other_11',['Other',['../namespacescxt_1_1import__support.html#a7ef0ab496f57e183b484e62e2053c94fa6311ae17c1ee52b36e68aaf4ad066387',1,'scxt::import_support']]],
-  ['output_5finfo_12',['output_info',['../namespacescxt_1_1messaging_1_1client.html#a2d1e2f23fe578da23e2809d174b8cb38a9c3719792f2bc8446dd0b9ff2692f983',1,'scxt::messaging::client']]],
-  ['output_5frouting_13',['OUTPUT_ROUTING',['../structscxt_1_1selection_1_1SelectionManager.html#ad013daf9867443d70831a8763cc78107ae5b28f146fd707f82d4156f376c72031',1,'scxt::selection::SelectionManager']]]
+  ['oneshot_5',['oneshot',['../structscxt_1_1modulation_1_1ModulatorStorage.html#a22823e777472311df6b822c4393c6127a2724fa87f252403cd2c93f7437f34fd5',1,'scxt::modulation::ModulatorStorage::ONESHOT'],['../structscxt_1_1modulation_1_1modulators_1_1AdsrStorage.html#a72379015e04e7913b42264153319af1cae875b67b322212bf81da51e697bc4450',1,'scxt::modulation::modulators::AdsrStorage::ONESHOT']]],
+  ['only_5fcollect_6',['ONLY_COLLECT',['../namespacescxt_1_1patch__io.html#a3581f6f1928496586a0cac5def142324abfcb04013527a1baf393ee4843c4f56e',1,'scxt::patch_io']]],
+  ['opus_5ffile_7',['OPUS_FILE',['../structscxt_1_1sample_1_1Sample.html#a3ae727773c367ac1041d72ac770a0ab1a839ddadb16cc4ed728d40f1d009df393',1,'scxt::sample::Sample']]],
+  ['or_8',['OR',['../structscxt_1_1engine_1_1GroupTriggerConditions.html#a4bf3727f5c3c89a9e7d3b88969ce767aa1d00e7dce692e8dc3f6877f035e3a616',1,'scxt::engine::GroupTriggerConditions']]],
+  ['or_5fnot_9',['OR_NOT',['../structscxt_1_1engine_1_1GroupTriggerConditions.html#a4bf3727f5c3c89a9e7d3b88969ce767aa756eaf1e3554bffb0df4a824191577f6',1,'scxt::engine::GroupTriggerConditions']]],
+  ['other_10',['Other',['../namespacescxt_1_1import__support.html#a7ef0ab496f57e183b484e62e2053c94fa6311ae17c1ee52b36e68aaf4ad066387',1,'scxt::import_support']]],
+  ['output_5finfo_11',['output_info',['../namespacescxt_1_1messaging_1_1client.html#a2d1e2f23fe578da23e2809d174b8cb38a9c3719792f2bc8446dd0b9ff2692f983',1,'scxt::messaging::client']]],
+  ['output_5frouting_12',['OUTPUT_ROUTING',['../structscxt_1_1selection_1_1SelectionManager.html#ad013daf9867443d70831a8763cc78107ae5b28f146fd707f82d4156f376c72031',1,'scxt::selection::SelectionManager']]]
 ];
