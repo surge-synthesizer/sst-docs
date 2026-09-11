e1a906ea8d65f3a52bb721d60c869773ce321430
baconpaul <paul@baconpaul.org>
2026-09-11 02:33:21 +0000

    Update doxygen for shortcircuit-xt from github action

--- docs/shortcircuit-xt/search/classes_c.js
+++ docs/shortcircuit-xt/search/classes_c.js
@@ -1,7 +1,8 @@
 var searchData=
 [
   ['narrowverticalmenu_0',['NarrowVerticalMenu',['../structscxt_1_1ui_1_1app_1_1shared_1_1NarrowVerticalMenu.html',1,'scxt::ui::app::shared']]],
-  ['noselection_1',['NoSelection',['../structscxt_1_1ui_1_1app_1_1edit__screen_1_1NoSelection.html',1,'scxt::ui::app::edit_screen']]],
-  ['noteexpressionsources_2',['NoteExpressionSources',['../structscxt_1_1voice_1_1modulation_1_1MatrixEndpoints_1_1Sources_1_1NoteExpressionSources.html',1,'scxt::voice::modulation::MatrixEndpoints::Sources']]],
-  ['numdenquantatt_3',['NumDenQuantAtt',['../structscxt_1_1ui_1_1app_1_1edit__screen_1_1MiscPanel_1_1NumDenQuantAtt.html',1,'scxt::ui::app::edit_screen::MiscPanel']]]
+  ['nestedsourcemenu_1',['NestedSourceMenu',['../structscxt_1_1ui_1_1app_1_1edit__screen_1_1NestedSourceMenu.html',1,'scxt::ui::app::edit_screen']]],
+  ['noselection_2',['NoSelection',['../structscxt_1_1ui_1_1app_1_1edit__screen_1_1NoSelection.html',1,'scxt::ui::app::edit_screen']]],
+  ['noteexpressionsources_3',['NoteExpressionSources',['../structscxt_1_1voice_1_1modulation_1_1MatrixEndpoints_1_1Sources_1_1NoteExpressionSources.html',1,'scxt::voice::modulation::MatrixEndpoints::Sources']]],
+  ['numdenquantatt_4',['NumDenQuantAtt',['../structscxt_1_1ui_1_1app_1_1edit__screen_1_1MiscPanel_1_1NumDenQuantAtt.html',1,'scxt::ui::app::edit_screen::MiscPanel']]]
 ];
