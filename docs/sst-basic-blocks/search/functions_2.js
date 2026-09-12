6d79d13ac95e05ab6277f512b35958793d42c54d
baconpaul <paul@baconpaul.org>
2026-09-12 20:01:36 +0000

    Update doxygen for sst-basic-blocks from github action

--- docs/sst-basic-blocks/search/functions_2.js
+++ docs/sst-basic-blocks/search/functions_2.js
@@ -1,15 +1,16 @@
 var searchData=
 [
   ['characterfilter_0',['characterfilter',['../structsst_1_1basic__blocks_1_1dsp_1_1CharacterFilter.html#a398075027ed9c13e69a82b00d23b0c3d',1,'sst::basic_blocks::dsp::CharacterFilter::CharacterFilter()'],['../structsst_1_1basic__blocks_1_1dsp_1_1CharacterFilter.html#aee961cd8ea41c8f5045f313ac0ace954',1,'sst::basic_blocks::dsp::CharacterFilter::CharacterFilter(SampleRateProvider *s) noexcept']]],
-  ['clamptopirange_1',['clampToPiRange',['../namespacesst_1_1basic__blocks_1_1dsp.html#ac709b283f3bb2a786d853870e075d5d9',1,'sst::basic_blocks::dsp']]],
-  ['clamptopirangesse_2',['clampToPiRangeSSE',['../namespacesst_1_1basic__blocks_1_1dsp.html#a6b643d7672ed0431fd549f761bd635b3',1,'sst::basic_blocks::dsp']]],
-  ['clear_3',['clear',['../structsst_1_1basic__blocks_1_1dsp_1_1SSESincDelayLine.html#ac8bb3912a3ce86b15842e79d0b421204',1,'sst::basic_blocks::dsp::SSESincDelayLine::clear()'],['../structsst_1_1basic__blocks_1_1dsp_1_1quadDelayLine.html#ac8bb3912a3ce86b15842e79d0b421204',1,'sst::basic_blocks::dsp::quadDelayLine::clear()']]],
-  ['clear_5fblock_4',['clear_block',['../namespacesst_1_1basic__blocks_1_1mechanics.html#a3f67fa7e95abef0f19cf72302ebd7b14',1,'sst::basic_blocks::mechanics']]],
-  ['copy_5ffrom_5fto_5',['copy_from_to',['../namespacesst_1_1basic__blocks_1_1mechanics.html#a93b9a6d9d2475305e9120051b44dc9c0',1,'sst::basic_blocks::mechanics']]],
-  ['copytorawarray_6',['copyToRawArray',['../structsst_1_1basic__blocks_1_1simd_1_1F32x4.html#ae1efe7f76265a0fa88ff477f67d0328a',1,'sst::basic_blocks::simd::F32x4']]],
-  ['correlated_5fnoise_5fo2mk2_5fsupplied_5fvalue_7',['correlated_noise_o2mk2_supplied_value',['../namespacesst_1_1basic__blocks_1_1dsp.html#ad431caeec17d8a36b30bb9921a312cae',1,'sst::basic_blocks::dsp']]],
-  ['correlated_5fnoise_5fo2mk2_5fsuppliedrng_8',['correlated_noise_o2mk2_suppliedrng',['../namespacesst_1_1basic__blocks_1_1dsp.html#a3f60d46ce011d4d89d74d2c9f9c36e2a',1,'sst::basic_blocks::dsp']]],
-  ['crossstagephasescale_9',['crossStagePhaseScale',['../structsst_1_1basic__blocks_1_1modulators_1_1AHDSRShapedSC.html#aadf611810df4e40b689c22245f1c25ac',1,'sst::basic_blocks::modulators::AHDSRShapedSC']]],
-  ['csip_10',['csip',['../structsst_1_1basic__blocks_1_1dsp_1_1EBOscillatorBase.html#acd55b08c52e0a32d07cd8d7b8e800ed9',1,'sst::basic_blocks::dsp::EBOscillatorBase']]],
-  ['cubic_5fipol_11',['cubic_ipol',['../namespacesst_1_1basic__blocks_1_1dsp.html#aba363ecc65489af3901abcf0954c48fd',1,'sst::basic_blocks::dsp']]]
+  ['chargeonset_1',['chargeOnset',['../structsst_1_1basic__blocks_1_1dsp_1_1EBApproxSemiSin.html#a2eb38a74bf99cc9d540a55c5983d4c67',1,'sst::basic_blocks::dsp::EBApproxSemiSin']]],
+  ['clamptopirange_2',['clampToPiRange',['../namespacesst_1_1basic__blocks_1_1dsp.html#ac709b283f3bb2a786d853870e075d5d9',1,'sst::basic_blocks::dsp']]],
+  ['clamptopirangesse_3',['clampToPiRangeSSE',['../namespacesst_1_1basic__blocks_1_1dsp.html#a6b643d7672ed0431fd549f761bd635b3',1,'sst::basic_blocks::dsp']]],
+  ['clear_4',['clear',['../structsst_1_1basic__blocks_1_1dsp_1_1SSESincDelayLine.html#ac8bb3912a3ce86b15842e79d0b421204',1,'sst::basic_blocks::dsp::SSESincDelayLine::clear()'],['../structsst_1_1basic__blocks_1_1dsp_1_1quadDelayLine.html#ac8bb3912a3ce86b15842e79d0b421204',1,'sst::basic_blocks::dsp::quadDelayLine::clear()']]],
+  ['clear_5fblock_5',['clear_block',['../namespacesst_1_1basic__blocks_1_1mechanics.html#a3f67fa7e95abef0f19cf72302ebd7b14',1,'sst::basic_blocks::mechanics']]],
+  ['copy_5ffrom_5fto_6',['copy_from_to',['../namespacesst_1_1basic__blocks_1_1mechanics.html#a93b9a6d9d2475305e9120051b44dc9c0',1,'sst::basic_blocks::mechanics']]],
+  ['copytorawarray_7',['copyToRawArray',['../structsst_1_1basic__blocks_1_1simd_1_1F32x4.html#ae1efe7f76265a0fa88ff477f67d0328a',1,'sst::basic_blocks::simd::F32x4']]],
+  ['correlated_5fnoise_5fo2mk2_5fsupplied_5fvalue_8',['correlated_noise_o2mk2_supplied_value',['../namespacesst_1_1basic__blocks_1_1dsp.html#ad431caeec17d8a36b30bb9921a312cae',1,'sst::basic_blocks::dsp']]],
+  ['correlated_5fnoise_5fo2mk2_5fsuppliedrng_9',['correlated_noise_o2mk2_suppliedrng',['../namespacesst_1_1basic__blocks_1_1dsp.html#a3f60d46ce011d4d89d74d2c9f9c36e2a',1,'sst::basic_blocks::dsp']]],
+  ['crossstagephasescale_10',['crossStagePhaseScale',['../structsst_1_1basic__blocks_1_1modulators_1_1AHDSRShapedSC.html#aadf611810df4e40b689c22245f1c25ac',1,'sst::basic_blocks::modulators::AHDSRShapedSC']]],
+  ['csip_11',['csip',['../structsst_1_1basic__blocks_1_1dsp_1_1EBOscillatorBase.html#acd55b08c52e0a32d07cd8d7b8e800ed9',1,'sst::basic_blocks::dsp::EBOscillatorBase']]],
+  ['cubic_5fipol_12',['cubic_ipol',['../namespacesst_1_1basic__blocks_1_1dsp.html#aba363ecc65489af3901abcf0954c48fd',1,'sst::basic_blocks::dsp']]]
 ];
--- docs/sst-basic-blocks/search/variables_d.js
+++ docs/sst-basic-blocks/search/variables_d.js
@@ -7,12 +7,13 @@ var searchData=
   ['one_4',['ONE',['../structsst_1_1basic__blocks_1_1dsp_1_1quadDelayLine.html#a1590fd923ce6cbb76719eec7fd1818b0',1,'sst::basic_blocks::dsp::quadDelayLine']]],
   ['onef_5',['ONEF',['../structsst_1_1basic__blocks_1_1dsp_1_1quadDelayLine.html#a1f170682a8a7d9223c7bc77630edbfa6',1,'sst::basic_blocks::dsp::quadDelayLine']]],
   ['oneovern_6',['oneOverN',['../structsst_1_1basic__blocks_1_1dsp_1_1RunningAverage.html#a9a4c3f355da3da74f01bcd27b1390c1e',1,'sst::basic_blocks::dsp::RunningAverage']]],
-  ['onto_7',['onto',['../structsst_1_1basic__blocks_1_1dsp_1_1LagCollection.html#a77604ce985ffd812475ee610987f923f',1,'sst::basic_blocks::dsp::LagCollection']]],
-  ['ordinalnumbering_8',['ordinalNumbering',['../structsst_1_1basic__blocks_1_1params_1_1ParamMetaData.html#a2192e65867b51a4f5be5a21f23d926ea',1,'sst::basic_blocks::params::ParamMetaData']]],
-  ['outblock0_9',['outBlock0',['../structsst_1_1basic__blocks_1_1modulators_1_1DiscreteStagesEnvelope.html#a98c667eb47a89575dd11ac786bf2474f',1,'sst::basic_blocks::modulators::DiscreteStagesEnvelope']]],
-  ['output_10',['output',['../structsst_1_1basic__blocks_1_1modulators_1_1DiscreteStagesEnvelope.html#a1084d7e47a5571b229a33ad347cc9453',1,'sst::basic_blocks::modulators::DiscreteStagesEnvelope::output'],['../structsst_1_1basic__blocks_1_1modulators_1_1StepLFO.html#a1084d7e47a5571b229a33ad347cc9453',1,'sst::basic_blocks::modulators::StepLFO::output']]],
-  ['outputblock_11',['outputBlock',['../structsst_1_1basic__blocks_1_1modulators_1_1SimpleLFO.html#ad5c9096b5f32c015b4882d39cc1966f0',1,'sst::basic_blocks::modulators::SimpleLFO']]],
-  ['outputcache_12',['outputCache',['../structsst_1_1basic__blocks_1_1modulators_1_1DiscreteStagesEnvelope.html#ab052e8cf7ad5ebfe770f8577db3af81d',1,'sst::basic_blocks::modulators::DiscreteStagesEnvelope']]],
-  ['outputcachecubed_13',['outputCacheCubed',['../structsst_1_1basic__blocks_1_1modulators_1_1DiscreteStagesEnvelope.html#a3f142fe566c46d572d9ac4129dd1eee4',1,'sst::basic_blocks::modulators::DiscreteStagesEnvelope']]],
-  ['outputcubed_14',['outputCubed',['../structsst_1_1basic__blocks_1_1modulators_1_1DiscreteStagesEnvelope.html#abc62f19a9752d21cad0625df584b0c6a',1,'sst::basic_blocks::modulators::DiscreteStagesEnvelope']]]
+  ['onsetchargepending_7',['onsetChargePending',['../structsst_1_1basic__blocks_1_1dsp_1_1EBOscillatorBase.html#a45b8823f45d5efe218d85f8281bba6a8',1,'sst::basic_blocks::dsp::EBOscillatorBase']]],
+  ['onto_8',['onto',['../structsst_1_1basic__blocks_1_1dsp_1_1LagCollection.html#a77604ce985ffd812475ee610987f923f',1,'sst::basic_blocks::dsp::LagCollection']]],
+  ['ordinalnumbering_9',['ordinalNumbering',['../structsst_1_1basic__blocks_1_1params_1_1ParamMetaData.html#a2192e65867b51a4f5be5a21f23d926ea',1,'sst::basic_blocks::params::ParamMetaData']]],
+  ['outblock0_10',['outBlock0',['../structsst_1_1basic__blocks_1_1modulators_1_1DiscreteStagesEnvelope.html#a98c667eb47a89575dd11ac786bf2474f',1,'sst::basic_blocks::modulators::DiscreteStagesEnvelope']]],
+  ['output_11',['output',['../structsst_1_1basic__blocks_1_1modulators_1_1DiscreteStagesEnvelope.html#a1084d7e47a5571b229a33ad347cc9453',1,'sst::basic_blocks::modulators::DiscreteStagesEnvelope::output'],['../structsst_1_1basic__blocks_1_1modulators_1_1StepLFO.html#a1084d7e47a5571b229a33ad347cc9453',1,'sst::basic_blocks::modulators::StepLFO::output']]],
+  ['outputblock_12',['outputBlock',['../structsst_1_1basic__blocks_1_1modulators_1_1SimpleLFO.html#ad5c9096b5f32c015b4882d39cc1966f0',1,'sst::basic_blocks::modulators::SimpleLFO']]],
+  ['outputcache_13',['outputCache',['../structsst_1_1basic__blocks_1_1modulators_1_1DiscreteStagesEnvelope.html#ab052e8cf7ad5ebfe770f8577db3af81d',1,'sst::basic_blocks::modulators::DiscreteStagesEnvelope']]],
+  ['outputcachecubed_14',['outputCacheCubed',['../structsst_1_1basic__blocks_1_1modulators_1_1DiscreteStagesEnvelope.html#a3f142fe566c46d572d9ac4129dd1eee4',1,'sst::basic_blocks::modulators::DiscreteStagesEnvelope']]],
+  ['outputcubed_15',['outputCubed',['../structsst_1_1basic__blocks_1_1modulators_1_1DiscreteStagesEnvelope.html#abc62f19a9752d21cad0625df584b0c6a',1,'sst::basic_blocks::modulators::DiscreteStagesEnvelope']]]
 ];
