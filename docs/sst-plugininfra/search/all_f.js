40fe7e0eaed55ef9a3e519612d4601014e76daec
baconpaul <paul@baconpaul.org>
2026-09-15 16:06:41 +0000

    Update doxygen for sst-plugininfra from github action

--- docs/sst-plugininfra/search/all_f.js
+++ docs/sst-plugininfra/search/all_f.js
@@ -10,6 +10,7 @@ var searchData=
   ['sst_3a_3aplugininfra_3a_3apatch_5fsupport_7',['patch_support',['../namespacesst_1_1plugininfra_1_1patch__support.html',1,'sst::plugininfra']]],
   ['sst_3a_3aplugininfra_3a_3apaths_8',['paths',['../namespacesst_1_1plugininfra_1_1paths.html',1,'sst::plugininfra']]],
   ['stacktracetostring_9',['stackTraceToString',['../namespacesst_1_1plugininfra_1_1misc__platform.html#af7413b8ebe9d6ecd40f7c42eaaa57860',1,'sst::plugininfra::misc_platform']]],
-  ['streamtoxml_10',['streamToXML',['../structsst_1_1plugininfra_1_1KeyMapManager.html#a24abfe7b30ea0535de9b8e75df4b7f11',1,'sst::plugininfra::KeyMapManager']]],
-  ['stringtoenum_11',['stringToEnum',['../structsst_1_1plugininfra_1_1KeyMapManager.html#a3ea05f0824b2827f3c58ecd76391e1bd',1,'sst::plugininfra::KeyMapManager']]]
+  ['streamdefaultbindings_10',['streamDefaultBindings',['../structsst_1_1plugininfra_1_1KeyMapManager.html#a82aa54e8eebfa409c1b1f7336c5be4f8',1,'sst::plugininfra::KeyMapManager']]],
+  ['streamtoxml_11',['streamToXML',['../structsst_1_1plugininfra_1_1KeyMapManager.html#a24abfe7b30ea0535de9b8e75df4b7f11',1,'sst::plugininfra::KeyMapManager']]],
+  ['stringtoenum_12',['stringToEnum',['../structsst_1_1plugininfra_1_1KeyMapManager.html#a3ea05f0824b2827f3c58ecd76391e1bd',1,'sst::plugininfra::KeyMapManager']]]
 ];
--- docs/sst-plugininfra/search/variables_a.js
+++ docs/sst-plugininfra/search/variables_a.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['stringtoenum_0',['stringToEnum',['../structsst_1_1plugininfra_1_1KeyMapManager.html#a3ea05f0824b2827f3c58ecd76391e1bd',1,'sst::plugininfra::KeyMapManager']]]
+  ['streamdefaultbindings_0',['streamDefaultBindings',['../structsst_1_1plugininfra_1_1KeyMapManager.html#a82aa54e8eebfa409c1b1f7336c5be4f8',1,'sst::plugininfra::KeyMapManager']]],
+  ['stringtoenum_1',['stringToEnum',['../structsst_1_1plugininfra_1_1KeyMapManager.html#a3ea05f0824b2827f3c58ecd76391e1bd',1,'sst::plugininfra::KeyMapManager']]]
 ];
